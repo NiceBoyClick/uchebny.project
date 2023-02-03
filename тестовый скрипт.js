a589fc2bc65bb86c85522238c17cd6d9531a0166
rotvsmetane <glass.ocean10@gmail.com>
2023-02-03 19:56:15 +0300

    33урок(Функция prompt)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,5 @@
 "use strict"
-let num1 = prompt('Введите первое число');
-let num2 = prompt('Введите второе число');
-
-alert(Number(num1) + Number(num2));
+let num1 = prompt('Введите сторону квадрата');
+alert('площадь квадрата: ' + (num1 * num1));
 
 
