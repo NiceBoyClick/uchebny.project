017a9a7b0c53c9f930d42991d21a3c9cb34b8fba
rotvsmetane <glass.ocean10@gmail.com>
2023-02-07 13:36:15 +0300

    50урок(Строковые ключи объектов в JavaScript)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,3 +1,3 @@
 "use strict"
-let obj = {1: 'январь', 2: 'февраль', 3: 'март', 4: "апрель", 5: "май", 6: "июнь", 7: "июль"};
-console.log(obj);
+let user = {name: 'Константин', surname: 'Королёв', patronymic: 'Сергеевич'};
+alert(user["name"] + ' ' + user["surname"] + ' ' + user["patronymic"])
