45e106ba11ebe34bf7c338912a17340dc982d6da
rotvsmetane <glass.ocean10@gmail.com>
2023-02-07 18:16:02 +0300

    55урок(Неупорядоченность объектов)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,3 +1,9 @@
 "use strict"
-let obj = {name: 'Константин', surname: 'Королёв' };
-console.log(obj)
+let obj = {
+    name: 'Константин',
+    surname: 'Королёв',
+    patronymic: 'Сергеевич',
+};
+console.log(obj['surname']);
+console.log(obj['name']);
+console.log(obj['patronymic']);
