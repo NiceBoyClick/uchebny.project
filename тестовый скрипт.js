4923a8d5c7165a72ab5387c5b0f1331ab365e732
rotvsmetane <glass.ocean10@gmail.com>
2023-02-07 15:00:24 +0300

    51урок(Ограничения на ключи объектов)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,3 +1,14 @@
 "use strict"
-let date = {year: 2023, month: 2, day: 7};
-alert(date.year + "-" +  date.month + "-" + date.day)
+let obj = {
+    '1a': 1,
+    'b2': 2,
+    'с-с': 3,
+    'd 4': 4,
+    'e5': 5
+};
+
+console.log(obj['1a']);
+console.log(obj.b2);
+console.log(obj['c-c']);
+console.log(obj['d 4']);
+console.log(obj.e5);
