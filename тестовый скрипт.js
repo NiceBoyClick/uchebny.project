4e1239d44ba0d69023e28c33b43e50836a345260
rotvsmetane <glass.ocean10@gmail.com>
2023-02-07 18:41:18 +0300

    56урок(Массив ключей объекта)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,4 @@
 "use strict"
-let obj = {
-    name: 'Константин',
-    surname: 'Королёв',
-    patronymic: 'Сергеевич',
-};
-console.log(obj['surname']);
-console.log(obj['name']);
-console.log(obj['patronymic']);
+let obj = {x: 1, y: 2, z: 3};
+let  keys = Object.keys(obj);
+console.log(keys);
