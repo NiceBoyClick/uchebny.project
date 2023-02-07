b0ff98e9d37656e6dd60aa18c80358207f3931ff
rotvsmetane <glass.ocean10@gmail.com>
2023-02-07 17:55:17 +0300

    54урок(Добавление элементов в объекты)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,6 +1,5 @@
 "use strict"
-let obj = {x: 1, y: 2, z: 3};
-obj['x'] = 1 ** 2;
-obj['y'] = 2 ** 2;
-obj['z'] = 3 ** 2;
+let obj = {};
+obj.name = 'Константин';
+obj.surname = 'Королёв';
 console.log(obj)
