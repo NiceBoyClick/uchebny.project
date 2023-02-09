d9c9f454a99c973eded21f3efaebcdd9af1caced
rotvsmetane <glass.ocean10@gmail.com>
2023-02-09 19:14:17 +0300

    80урок(Неравенство по значению и типу)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,9 @@
-"use strict"
-let test1 = '123';
-let test2 = 123;
+let test1 = '3';
+let test2 = '3';
 
-if ('123' === '123') {
+if (test1 != test2) {
     console.log('+++');
 } else {
-    console.log('---')
+    console.log('---');
 }
+
