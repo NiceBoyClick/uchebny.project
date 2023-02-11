90a617f6f9721ab31337655cccde8d3590137ae7
rotvsmetane <glass.ocean10@gmail.com>
2023-02-11 12:40:06 +0300

    85 урок (Инвертирование логических выражений)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,7 @@
 "use strict"
-let num = 3;
-
-if ((num > 5 && num < 10) || (num == 20)) {
+let num1 = 0;
+let num2 = 11;
+if (!(num1 >= 0 || num2 <= 10)) {
     console.log('+++');
 } else {
     console.log('---');
