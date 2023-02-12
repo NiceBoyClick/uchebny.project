a04d3475405a70ab6ad038c82684132b06b85251
rotvsmetane <glass.ocean10@gmail.com>
2023-02-12 16:16:07 +0300

    94 урок (Конструкция else if)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,6 +1,12 @@
 "use strict"
-if (test > 0)
-    console.log('+++');
-else
-    console.log('---');
+let day = 19;
 
+if (day >= 1 && day <= 10) {
+    console.log('value1');
+} else if (day >= 11 && day <= 20) {
+    console.log('value2');
+} else if (day >= 21 && day <= 31) {
+    console.log('value3');
+} else {
+    console.log('неверное значение переменной  day');
+}
