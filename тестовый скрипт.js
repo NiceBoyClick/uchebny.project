81815f4b76c536e208ad94615be06048e03dc5ef
rotvsmetane <glass.ocean10@gmail.com>
2023-02-13 13:36:16 +0300

    98 урок (Тернарный оператор)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,11 +1,4 @@
 "use strict"
-let num = 1; // пусть здесь число 1
-
-switch (num) {
-    case 1:
-        console.log(1);
-    case 2:
-        console.log(2);
-    case 3:
-        console.log(3);
-}
+let num = 1;
+let res = num >= 0 ? 1: 2;
+console.log(res)
