52072c6eeb2946c61bf868a0f5bc1e284f9ef59a
rotvsmetane <glass.ocean10@gmail.com>
2023-02-13 19:44:20 +0300

    103 урок (Проверка частей часа через if-else)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,11 +1,14 @@
 "use strict"
-let age = 15;
-let adult;
+let min = 44;
 
-if (age >= 18) {
-     adult = true;
-} else {
-      adult = false;
+if (min >= 0 && min <= 19) {
+     console.log('1 треть');
 }
 
-console.log(adult);
+if (min >= 20 && min <= 39) {
+     console.log('2 треть');
+}
+
+if (min >= 40 && min <= 60) {
+     console.log('3 треть');
+}
