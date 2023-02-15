63d05d39134dea249a0762dc8df7d308723bfaf7
rotvsmetane <glass.ocean10@gmail.com>
2023-02-15 14:15:39 +0300

    109 урок (Практика на условия if-else)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,5 +1,9 @@
 "use strict"
-let num = 433;
-let str = String(num)
-let sum = Number(str[0]) +  Number(str[1]) + Number(str[2]);
-alert(sum);
+let num = '311111';
+let sum1 = Number(num[0]) +  Number(num[1]) + Number(num[2]);
+let sum2 = Number(num[3]) +  Number(num[4]) + Number(num[5]);
+if (sum1 == sum2) {
+     console.log('да');
+} else {
+     console.log('нет')
+}
