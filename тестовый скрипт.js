87dafb9b77546dbbc0ef9c41472cea6f74e16271
rotvsmetane <glass.ocean10@gmail.com>
2023-02-15 14:02:52 +0300

    109 урок (Практика на условия if-else)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,14 +1,8 @@
 "use strict"
-let month = 7;
-if (month == 1 || month == 2 || month == 12) {
-     console.log('зима');
-}
-if (month >= 3 && month <= 5 ) {
-     console.log('весна');
-}
-if (month >=6 && month <=8) {
-     console.log('лето');
-}
-if (month >=9 && month <= 11) {
-     console.log('осень');
+let num = 12345;
+let str = String(num)
+if (str[0] == 1 || str[0] == 2 || str[0] == 3) {
+     console.log('да');
+} else {
+     console.log('нет')
 }
