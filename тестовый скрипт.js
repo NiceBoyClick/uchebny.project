69ab78987ae87e42c0300ead8750dfbd5b17a14b
rotvsmetane <glass.ocean10@gmail.com>
2023-02-15 20:04:52 +0300

    111 урок (for loop)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,4 @@
 "use strict"
-let num = '311111';
-let sum1 = Number(num[0]) +  Number(num[1]) + Number(num[2]);
-let sum2 = Number(num[3]) +  Number(num[4]) + Number(num[5]);
-if (sum1 == sum2) {
-     console.log('да');
-} else {
-     console.log('нет')
+for (let i = 1; i <= 99; i+=2) {
+     console.log(i);
 }
