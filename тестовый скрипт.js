8c6c1fb91abc1e3fab31e6e6ce96c78d40b2bc00
rotvsmetane <glass.ocean10@gmail.com>
2023-02-16 13:20:18 +0300

    116 урок (Условия в циклах)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,8 @@
 "use strict"
-let arr = [2, 5, 9, 15, 1, 4];
-for (let elem of arr) {
-    if (elem > 3 && elem < 10) {
-        console.log(elem);
+let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+for (let key in obj) {
+    if (obj[key] % 2 !== 0) {
+        console.log(obj[key]);
     }
 }
 
