8bde5b3d0035d077be67ebdf2716aaa272656108
rotvsmetane <glass.ocean10@gmail.com>
2023-02-19 12:29:44 +0300

    123 урок (Инструкция break) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,11 +1,7 @@
 "use strict"
-let pos = 0;
-let arr = [6, 7, 5, 3, 4, 1, 9];
-for (let elem of arr) {
-    pos += 1;
-    if (elem === 3) {
-        console.log(pos)
-        break;
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+        document.write(i + (j + ' '));
     }
 }
 
