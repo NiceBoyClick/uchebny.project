eb18d8d885bcc18acdd8a6c3a2e1783b80f325d6
rotvsmetane <glass.ocean10@gmail.com>
2023-02-19 18:54:04 +0300

    126 урок (Заполнение массива) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,11 @@
 "use strict"
-for (let i = 1; i <= 3; i++) {
-    for (let j = 1; j <= 3; j++) {
-        document.write(i + (j + ' '));
-    }
+let arr = [];
+
+for (let i = 1; i <= 10; i++) {
+    arr.push(i);
 }
+
+console.log(arr);
 
 
 
