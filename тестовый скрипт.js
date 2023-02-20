6f94029048306fac39050cc2af8a150930b58f32
rotvsmetane <glass.ocean10@gmail.com>
2023-02-20 20:03:35 +0300

    128 урок (Заполнение объектов через цикл) №3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,12 +3,9 @@ let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
 let obj2 = {};
 
 for (let key in obj) {
-    if (obj[key] % 2 == 0) {
-        obj2[key] = obj[key];
-    }
+    obj2[obj[key]] = key;
 }
-console.log(obj2)
-
+console.log(obj2);
 
 
 
