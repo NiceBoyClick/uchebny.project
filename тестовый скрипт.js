ce97d653920cfc9ea4936e33578f5075cc46ba26
rotvsmetane <glass.ocean10@gmail.com>
2023-02-20 13:31:55 +0300

    128 урок (Заполнение объектов через цикл) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,13 @@
 "use strict"
-let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-let arr2 = [1, 2, 3, 4, 5, 6, 7];
-let obj = {};
-for (let i = 0; i <= 6; i++) {
-    obj[arr1[i]] = arr2[i];
-}
-console.log(obj)
+let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+let obj2 = {};
 
+for (let key in obj) {
+    if (obj[key] % 2 == 0) {
+        obj2[key] = obj[key];
+    }
+}
+console.log(obj2)
 
 
 
