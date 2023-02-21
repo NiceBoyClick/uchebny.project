60e813e67a4cdd31900d9e6d7ceea29db72d2a50
rotvsmetane <glass.ocean10@gmail.com>
2023-02-21 13:42:26 +0300

    130 урок (Изменение объектов через цикл) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,10 +1,19 @@
 "use strict"
-let obj = {x: 1, y: 2, z: 3};
-for (let key in obj) {
-    obj[key] = obj[key] + 1;
-}
-console.log(obj);
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let flag = false;
 
+for (let elem of arr) {
+    if (elem == 'c') {
+        flag = true;
+        break;
+    }
+}
+
+if (flag == true) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
 
 
 
