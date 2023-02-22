8eec375de3083103aeee3489c3baea0c799f20e4
rotvsmetane <glass.ocean10@gmail.com>
2023-02-22 14:38:57 +0300

    131 урок (Подсчет количества элементов в массиве) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,17 @@
 "use strict"
 let arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
-let counter = 0;
+let counter3 = 0;
+let counter2 = 0;
 for (let elem of arr) {
     if (elem == 3) {
-        counter++;
+        counter3++;
+    }
+    if (elem == 2) {
+      counter2++;
     }
 }
-console.log(counter);
+console.log(`количество двоек: ${counter2}`);
+console.log(`количество троек: ${counter3}`);
 
 
 
