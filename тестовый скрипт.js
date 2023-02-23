cf482756b3b74c90882684e68c27042e3a8a9f06
rotvsmetane <glass.ocean10@gmail.com>
2023-02-23 20:01:23 +0300

    134 урок (Советы по написанию кода циклов)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,11 @@
 "use strict"
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (let i = 1; i < arr.length - 1; i++) {
-    console.log(arr[i - 1] + arr[i] + arr[i + 1]);
+let arr = [10, 20, 30, 40, 21, 32, 51];
+let sum = 0;
+for (let elem of arr) {
+    let str = String(elem);
+    if (str[0] == 1 || str[0] == 2) {
+        sum += Number(str);
+    }
 }
-
+console.log(sum);
 
