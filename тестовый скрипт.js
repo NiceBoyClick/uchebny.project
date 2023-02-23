09ee93834ef3da5ddcd644b45d7c2d81f2bfbc7c
rotvsmetane <glass.ocean10@gmail.com>
2023-02-23 13:41:48 +0300

    133 урок (Получение соседей элементов в массиве) №4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,8 @@
 "use strict"
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (let i = 2; i < arr.length; i++) {
-    console.log(arr[i - 1] + ' ' + arr[i - 2]);
+let sum = 0;
+for (let i = 2; i <= arr.length; i++) {
+    console.log((i - 2 + i - 1) + i);
 }
 
 
