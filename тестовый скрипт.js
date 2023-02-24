9b1139833e1551ca40a14e73770310687f4e90c4
rotvsmetane <glass.ocean10@gmail.com>
2023-02-24 20:45:38 +0300

    137 урок (Отработка циклов)№10

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,7 @@
 "use strict"
-let num = [2, 13, 7, 18, 5, 3, 0];
+let num = [2, 13, 7, 18, 5, 3];
 let sum = 0;
 for (let i = 0; i <= num.length - 1; i++) {
-    sum += num[i];
+    sum += num[i] / num.length;
 }
 console.log(sum)
-
