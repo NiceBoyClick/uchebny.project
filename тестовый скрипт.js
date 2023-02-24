b07e4288f50f5d177bb5d768f10f42bf0c99c9bd
rotvsmetane <glass.ocean10@gmail.com>
2023-02-24 20:23:25 +0300

    137 урок (Отработка циклов)№8

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,20 +1,8 @@
 "use strict"
 let num = [2, 13, 7, 18, 5, 3, 0];
-let flag = false;
-for (let i = 0; i <= num.length; i++) {
-    if (num[i] === 5) {
-        flag = true;
-        break;
-    } else {
-        flag = false;
-    }
+let sum = 0;
+for (let i = 0; i <= num.length - 1; i++) {
+    sum += num[i];
 }
-if (flag === true) {
-    console.log('+++')
-} else {
-    console.log('---')
-}
-
-
-
+console.log(sum)
 
