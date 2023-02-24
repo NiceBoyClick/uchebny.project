166b94ba54b5c1a3334dac988f0c8e66510bd213
rotvsmetane <glass.ocean10@gmail.com>
2023-02-24 12:58:52 +0300

    136 урок (Поиск ошибок в коде с циклами)

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,5 @@
 "use strict"
-let obj = {a: 10, b: 20, c: 30, d: 40, e: 50};
-let sum = 0;
-
-for (let elem in obj) {
-    let str = String(obj[elem]);
-    if (str[0] === '1' || str[0] === '2') {
-        sum += Number(str);
-    }
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
 }
-console.log(sum);
 
