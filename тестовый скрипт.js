fc011902249d0e1ce098dd01997ea253adb70a18
rotvsmetane <glass.ocean10@gmail.com>
2023-02-26 19:40:41 +0300

    137 урок (Отработка циклов)№15

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,5 @@
 "use strict"
 let arr = [10, 20, 30, 50, 235, 3000];
-for (let i = 0; i <= arr.length; i++) {
-    let str = String(arr[i]);
-    if (str[0] == 1 || str[0] == 2 || str[0] == 5) {
-        console.log(str);
-    }
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i])
 }
