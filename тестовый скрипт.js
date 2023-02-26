a8edbcf4417281ac3d945af6d1d26e2f5bcae19c
rotvsmetane <glass.ocean10@gmail.com>
2023-02-26 20:33:15 +0300

    137 урок (Отработка циклов)№20

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,9 @@
 "use strict"
 let arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let day = 6;
 for (let i = 0; i <= arr.length - 1; i++) {
-    if (i == arr.length - 1 || i == arr.length - 2) {
-        document.write(`<br><b>${arr[i]}</b><br>`)
+    if (i == day) {
+        document.write(`<br><i>${arr[i]}</i><br>`)
     } else {
         document.write(`<br>${arr[i]}<br>`)
     }
