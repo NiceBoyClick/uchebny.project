fcf839e764d36089977bfe13ed32e16c39602620
rotvsmetane <glass.ocean10@gmail.com>
2023-02-26 20:08:04 +0300

    137 урок (Отработка циклов)№18

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,6 +1,6 @@
 "use strict"
 let arr = [10, 1, 2, 50, 235, 3000];
 for (let i = 0; i <= arr.length - 1; i++) {
-    document.write(arr[i]);
+    document.write(`<p>${arr[i]}</p>`);
     document.write('<br>');
 }
