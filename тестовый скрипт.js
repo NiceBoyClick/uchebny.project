401c9ec120f70fec1a2d09480cf0c75101fb5902
rotvsmetane <glass.ocean10@gmail.com>
2023-02-26 15:21:46 +0300

    137 урок (Отработка циклов)№13

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,6 +1,9 @@
 "use strict"
-let arr = [];
-for (let i = 10; i >= 1; i--) {
-    arr.push(i);
+let arr = [2, -1, 6, 1, -4, 3];
+let sum = 0;
+for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] > 0) {
+        sum += arr[i];
+    }
 }
-console.log(arr);
+console.log(sum);
