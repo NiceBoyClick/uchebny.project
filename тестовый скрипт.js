65c85fe3be02ded5d9033ca083f855b84e3936c2
rotvsmetane <glass.ocean10@gmail.com>
2023-02-27 19:03:45 +0300

    138 урок (Отработка циклов)№1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,10 +1,11 @@
 "use strict"
-let arr = ['a', 'b', 'c', 'd', 'e'];
-let obj = {};
-for (let i = 0; i <= arr.length - 1; i++) {
-    let keys = i + 1;
-    let values = arr[i];
-    obj[keys] = values;
-}
-console.log(obj);
-
+let arr = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['j', 'k', 'l'],
+];
+console.log(arr[3][2]);
+console.log(arr[1][1]);
+console.log(arr[2][0]);
+console.log(arr[0][0]);
