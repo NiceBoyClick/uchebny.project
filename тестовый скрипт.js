a604fb551db1693477e5910448c1c64193873285
rotvsmetane <glass.ocean10@gmail.com>
2023-02-27 14:22:19 +0300

    137 урок (Отработка циклов)№27

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,20 +1,9 @@
 "use strict"
-let obj = {
-    1: 125,
-    2: 225,
-    3: 128,
-    4: 356,
-    5: 145,
-    6: 281,
-    7: 452,
-};
-let mass = [];
-for (let elem in obj) {
-    let i = obj[elem];
-    i = String(obj[elem]);
-    if (i[0] == 1 || i[0] == 2) {
-        mass.push(+i);
-    }
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let obj = {};
+for (let i = 0; i <= arr.length - 1; i++) {
+    let keys = arr[i];
+    obj[keys] = i + 1;
 }
-console.log(mass);
+console.log(obj);
 
