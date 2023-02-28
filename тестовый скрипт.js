3209a1049bf90959655fd693da6d36e151195d5f
rotvsmetane <glass.ocean10@gmail.com>
2023-02-28 16:41:42 +0300

    140 урок (Заполнение многомерных массивов)№1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,20 +1,9 @@
 "use strict"
-let arr = [[1, 2, 3], [4, 5], [6]];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        sum += arr[i][j];
+let arr = [];
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+    for (let j = 0; j < 5; j++) {
+        arr[i].push(j + 1);
     }
 }
-console.log(sum);
-
-let arr2 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
-let sum2 = 0;
-for (let i = 0; i < arr2.length; i++) {
-    for (let j = 0; j < arr2[i].length; j++) {
-        for (let n = 0; n < arr2[i][j].length; n++) {
-            sum2 += arr2[i][j][n];
-        }
-    }
-}
-console.log(sum2);
+console.log(arr);
