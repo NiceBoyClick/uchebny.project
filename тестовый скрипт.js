dc864fc2921222186fd1c31b7be4d386334f2d04
rotvsmetane <glass.ocean10@gmail.com>
2023-02-28 17:42:27 +0300

    140 урок (Заполнение многомерных массивов)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -2,8 +2,11 @@
 let arr = [];
 for (let i = 0; i < 3; i++) {
     arr[i] = [];
-    for (let j = 0; j < 4; j++) {
-        arr[i].push('x');
+    for (let j = 0; j < 2; j++) {
+        arr[i].push([]);
+        for (let n = 0; n < 5; n++) {
+            arr[i][j].push(n + 1);
+        }
     }
 }
 console.log(arr);
