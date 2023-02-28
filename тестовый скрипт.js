4ac0ee5c9b5e414ab16a249f1381284f53347b67
rotvsmetane <glass.ocean10@gmail.com>
2023-02-28 13:52:09 +0300

    139 урок (Перебор многомерных массивов)№2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,11 @@
 "use strict"
-let arr = [[1, 2, 3], [4, 5], [6]];
+let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 let sum = 0;
 for (let subArr of arr) {
-    for (let elem of subArr) {
-        sum += elem;
+    for (let subsubArr of subArr) {
+        for (let elem of subsubArr) {
+            sum += elem;
+        }
     }
 }
 console.log(sum);
