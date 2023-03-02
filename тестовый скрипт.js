979ae80277c3c75516c6c2a03b487f2fe1756d03
rotvsmetane <glass.ocean10@gmail.com>
2023-03-02 19:36:44 +0300

    143 урок (Многомерные объекты)№1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,27 @@
 "use strict"
-let arr = [];
-
-for (let i = 0, k = 1; i < 3; i++) {
-    arr[i] = [];
-
-    for (let j = 0; j < 3; j++) {
-        arr[i][j] = k++;
-    }
+let obj = {
+    key1: {
+        key1: 1,
+        key2: 2,
+        key3: 3,
+    },
+    key2: {
+        key1: 4,
+        key2: 5,
+        key3: 6,
+    },
+    key3: {
+        key1: 7,
+        key2: 8,
+        key3: 9,
+    },
 }
-
-console.log(arr);
+console.log(obj["key1"]["key1"]
+    + obj["key1"]["key2"]
+    + obj["key1"]["key3"]
+    + obj["key2"]["key1"]
+    + obj["key2"]["key2"]
+    + obj["key2"]["key3"]
+    + obj["key3"]["key1"]
+    + obj["key3"]["key2"]
+    + obj["key3"]["key3"]);
