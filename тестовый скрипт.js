84f751823fd56852ca341bc17bc1336a5c69fe25
rotvsmetane <glass.ocean10@gmail.com>
2023-03-02 15:37:22 +0300

    142 урок (Заполнение многомерных массивов по порядку)№8

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,11 @@
 "use strict"
 let arr = [];
-let k = 1;
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0, k = 1; i < 3; i++) {
     arr[i] = [];
 
     for (let j = 0; j < 3; j++) {
-        arr[i][j] = k;
-        k++;
+        arr[i][j] = k++;
     }
 }
 
