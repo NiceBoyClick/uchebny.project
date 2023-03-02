949ef18f97ebfbdb0a80400ae41d691a1768ae4c
rotvsmetane <glass.ocean10@gmail.com>
2023-03-02 20:34:04 +0300

    144 урок (Перебор многомерных объектов)№2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,19 +1,40 @@
 "use strict"
 let obj = {
     1: {
-        1: 11,
-        2: 12,
-        3: 13,
+        1: {
+            1: 111,
+            2: 112,
+            3: 113,
+        },
+        2: {
+            1: 121,
+            2: 122,
+            3: 123,
+        },
     },
     2: {
-        1: 21,
-        2: 22,
-        3: 23,
+        1: {
+            1: 211,
+            2: 212,
+            3: 213,
+        },
+        2: {
+            1: 221,
+            2: 222,
+            3: 223,
+        },
     },
     3: {
-        1: 24,
-        2: 25,
-        3: 26,
+        1: {
+            1: 311,
+            2: 312,
+            3: 313,
+        },
+        2: {
+            1: 321,
+            2: 322,
+            3: 323,
+        },
     },
 }
 let sum = 0;
@@ -21,7 +42,12 @@ for (let key in obj) {
     let subObj = obj[key];
 
     for (let subKey in subObj) {
-        sum += subObj[subKey];
+        let subsubObj = subObj[subKey];
+
+        for (let subsubKey in subsubObj) {
+            sum += subsubObj[subsubKey];
+        }
     }
 }
 console.log(sum);
+
