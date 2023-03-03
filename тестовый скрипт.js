d39f22f13a16ee425890f49aeb9f029b50f1419b
rotvsmetane <glass.ocean10@gmail.com>
2023-03-03 15:48:31 +0300

    147 урок (Массив объектов)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,19 +3,34 @@ let employees = [
     {
         name: 'name1',
         salary: 300,
+        age: 28,
     },
     {
         name: 'name2',
         salary: 400,
+        age: 29,
     },
     {
         name: 'name3',
         salary: 500,
+        age: 30,
+    },
+    {
+        name: 'name4',
+        salary: 600,
+        age: 31,
+    },
+    {
+        name: 'name5',
+        salary: 700,
+        age: 32,
     },
 ];
 let sum = 0;
 for (let employee of employees) {
-    sum += employee.salary
+    if (employee.age >= 30) {
+        sum += employee.salary;
+    }
 }
 console.log(sum);
 
