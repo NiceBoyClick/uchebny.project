5336750c78fd96e078cf7caa0b033a75396c2417
rotvsmetane <glass.ocean10@gmail.com>
2023-03-03 20:09:57 +0300

    150 урок (Добавление элементов в многомерные объекты)№2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,26 +1,10 @@
 "use strict"
-let employees = [
-    {
-        name: 'name1',
-        salary: 300,
-        age: 28,
-    },
-    {
-        name: 'name2',
-        salary: 400,
-        age: 29,
-    },
-    {
-        name: 'name3',
-        salary: 500,
-        age: 30,
-    },
-];
-employees.push({
-    name: 'name4',
-    salary: 600,
-    age: 40,
-});
-console.log(employees);
+let affairs = {
+    '2019-12-28': ['data11', 'data12', 'data13'],
+    '2019-12-29': ['data21', 'data22', 'data23'],
+    '2019-12-30': ['data31', 'data32', 'data33'],
+}
+affairs["2019-12-29"].push('data24');
+console.log(affairs);
 
 
