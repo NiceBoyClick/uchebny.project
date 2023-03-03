c716afefb031bd5a52c6d0722d5e21a64b2af9f2
rotvsmetane <glass.ocean10@gmail.com>
2023-03-03 15:24:39 +0300

    146 урок (Перебор многомерных структур)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,24 +1,48 @@
 "use strict"
 let data = [
     {
-        1: 'data11',
-        2: 'data12',
-        3: 'data13',
+        1: [
+            'data111',
+            'data112',
+            'data113',
+        ],
+        2: [
+            'data121',
+            'data122',
+            'data123',
+        ],
     },
     {
-        1: 'data21',
-        2: 'data22',
-        3: 'data33',
+        1: [
+            'data211',
+            'data212',
+            'data213',
+        ],
+        2: [
+            'data221',
+            'data222',
+            'data223',
+        ],
     },
     {
-        1: 'data31',
-        2: 'data32',
-        3: 'data33',
+        1: [
+            'data411',
+            'data412',
+            'data413',
+        ],
+        2: [
+            'data421',
+            'data422',
+            'data423',
+        ],
     },
 ];
 for (let obj of data) {
-    for (let name in obj) {
-       console.log(obj[name]);
+    for (let arr in obj) {
+        let subarr = obj[arr];
+        for (let name of subarr) {
+            console.log(name);
+        }
     }
 }
 
