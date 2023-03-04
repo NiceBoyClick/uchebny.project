87a887defe2b356b7cd65f6fe152c7adb4afd114
rotvsmetane <glass.ocean10@gmail.com>
2023-03-04 15:13:58 +0300

    151 урок (Математические методы, работа со степенью и корнем)№1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,24 +1,4 @@
 "use strict"
-let students = {
-    'group1': {
-        'subgroup11': ['student111', 'student112', 'student113'],
-        'subgroup12': ['student121', 'student122', 'student123'],
-    },
-    'group2': {
-        'subgroup21': ['student211', 'student212', 'student213'],
-        'subgroup22': ['student221', 'student222', 'student223'],
-    },
-    'group3': {
-        'subgroup31': ['student311', 'student312', 'student313'],
-        'subgroup32': ['student321', 'student322', 'student323'],
-    },
-};
-
-students['group1']['subgroup11'].push('student114');
-students['group1']['subgroup13'] = [];
-students['group4'] = {};
-students['group4']['subgroup41'] = []
-students['group4']['subgroup41'].push('student411', 'student422');
-console.log(students);
+console.log(Math.pow(2, 10));
 
 
