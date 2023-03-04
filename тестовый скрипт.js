ab32cee1f01962a1d267d74512ec45d870b797e5
rotvsmetane <glass.ocean10@gmail.com>
2023-03-04 14:18:12 +0300

    150 урок (Добавление элементов в многомерные объекты)№5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,20 @@
 "use strict"
-let affairs = {
-    '2019-12-28': ['data11', 'data12', 'data13'],
-    '2019-12-29': ['data21', 'data22', 'data23'],
-    '2019-12-30': ['data31', 'data32', 'data33'],
+let students = {
+    'group1': {
+        'subgroup11': ['student111', 'student112', 'student113'],
+        'subgroup12': ['student121', 'student122', 'student123'],
+    },
+    'group2': {
+        'subgroup21': ['student211', 'student212', 'student213'],
+        'subgroup22': ['student221', 'student222', 'student223'],
+    },
+    'group3': {
+        'subgroup31': ['student311', 'student312', 'student313'],
+        'subgroup32': ['student321', 'student322', 'student323'],
+    },
 };
 
-affairs['2019-12-29'].push('data24');
-affairs['2019-12-31'] = [];
-affairs['2019-12-31'].push('data41');
-console.log(affairs);
+students['group1']['subgroup11'].push('student114');
+console.log(students);
 
 
