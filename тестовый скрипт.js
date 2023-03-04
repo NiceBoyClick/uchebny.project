dc0839a429915ad011336fff87dee2d55bd26b76
rotvsmetane <glass.ocean10@gmail.com>
2023-03-04 14:13:32 +0300

    150 урок (Добавление элементов в многомерные объекты)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,8 +3,11 @@ let affairs = {
     '2019-12-28': ['data11', 'data12', 'data13'],
     '2019-12-29': ['data21', 'data22', 'data23'],
     '2019-12-30': ['data31', 'data32', 'data33'],
-}
-affairs["2019-12-29"].push('data24');
+};
+
+affairs['2019-12-29'].push('data24');
+affairs['2019-12-31'] = [];
+affairs['2019-12-31'].push('data41');
 console.log(affairs);
 
 
