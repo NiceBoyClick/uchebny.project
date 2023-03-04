c016ac85c99b5ea6490ea67c914ec2375f154e1e
rotvsmetane <glass.ocean10@gmail.com>
2023-03-04 15:32:33 +0300

    151 урок (Математические методы, работа со степенью и корнем)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,4 +1,12 @@
 "use strict"
-console.log(Math.sqrt(245));
+ let arr =  [4, 2, 5, 19, 13, 0, 10];
+let sum = 0;
+let square = 0;
+for (let i = 0; i < arr.length; i++) {
+   sum += Math.pow(arr[i], 3);
+   square = Math.sqrt(sum);
+}
+console.log(sum);
+console.log(square);
 
 
