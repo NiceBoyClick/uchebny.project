e36dd4e06a9153fa0c099c25642b009575a0c152
rotvsmetane <glass.ocean10@gmail.com>
2023-03-05 13:33:34 +0300

    151 урок (Работа с функциями округления)№4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,6 @@
 "use strict"
- let arr =  [4, 2, 5, 19, 13, 0, 10];
-let sum = 0;
-let square = 0;
-for (let i = 0; i < arr.length; i++) {
-   sum += Math.pow(arr[i], 3);
-   square = Math.sqrt(sum);
-}
-console.log(sum);
-console.log(square);
-
+let sqr = Math.sqrt(379);
+console.log(Math.round(sqr));
+console.log(+sqr.toFixed(1));
+console.log(+sqr.toFixed(2));
 
