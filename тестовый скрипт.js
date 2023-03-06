5703e2895c23bd4516cc678b0a69bac7866400ec
rotvsmetane <glass.ocean10@gmail.com>
2023-03-06 12:46:03 +0300

    151 урок (Работа с рандомом)№8

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,6 +1,9 @@
 "use strict"
+let arr = [];
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-console.log(getRandomInt(1, 100));
+for (let i = 0; i < 10; i++) {
+    arr.push(getRandomInt(1, 100))
+}
+console.log(arr);
