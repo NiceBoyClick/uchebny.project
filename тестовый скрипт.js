b815a4b0dc6a770a73ab4958ccbecf3e802b3399
rotvsmetane <glass.ocean10@gmail.com>
2023-03-07 15:41:40 +0300

    152 урок (Строковые методы. Работа с indexOf)№4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,24 +1,3 @@
 "use strict"
-let str =  'я учу javascript!';
-let sub = str.substring(0, 5);
-console.log(sub.toLowerCase());
-
-let str1 =  'я учу javascript!';
-let sub1 = str.substr(0, 5);
-console.log(sub1.toLowerCase());
-
-let str2 =  'я учу javascript!';
-let sub2 = str.slice(0, 5);
-console.log(sub2.toLowerCase());
-
-let str3 =  'я учу javascript!';
-let sub3 = str.substring(6);
-console.log(sub3.toLowerCase());
-
-let str4 =  'я учу javascript!';
-let sub4 = str.substr(-11);
-console.log(sub4.toLowerCase());
-
-let str5 =  'я учу javascript!';
-let sub5 = str.slice(-11);
-console.log(sub5.toLowerCase());
+let str =  'abcde';
+console.log(str.indexOf('c'));
