2a27f216788a76bcba3e152876eeb12d7d5fe2d0
rotvsmetane <glass.ocean10@gmail.com>
2023-03-08 20:52:32 +0300

    153 урок (Методы для массивов, Работа с indexOf)№10

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,6 +1,4 @@
 "use strict"
 let arr = [1, 2, 3, 4, 5];
-let sub = arr.splice(1, 0, 'a', 'b');
-let sub1 = arr.splice(5, 0, 'c');
-let sub2 = arr.splice(7, 0, 'e');
-console.log(arr);
+let num = arr.indexOf(3);
+console.log(num);
