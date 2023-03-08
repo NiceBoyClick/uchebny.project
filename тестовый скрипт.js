62ae9d71f291af0cef05289f84c74db539855367
rotvsmetane <glass.ocean10@gmail.com>
2023-03-08 20:35:28 +0300

    153 урок (Методы для массивов, Работа с splice)№9

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,4 +1,6 @@
 "use strict"
 let arr = [1, 2, 3, 4, 5];
-let sub = arr.splice(3, 0, 'a', 'b', 'c');
+let sub = arr.splice(1, 0, 'a', 'b');
+let sub1 = arr.splice(5, 0, 'c');
+let sub2 = arr.splice(7, 0, 'e');
 console.log(arr);
