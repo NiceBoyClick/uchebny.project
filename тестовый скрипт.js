2f1eb87dbcbd7b4685acdfdfc17bd302edfb82e1
rotvsmetane <glass.ocean10@gmail.com>
2023-03-09 16:42:59 +0300

    154 урок (Практика на использования изученных методов           Слова в строке)№4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,5 +1,8 @@
 "use strict"
-let str = 'London';
-
-let result = str.slice(0, 1).toLowerCase() + str.slice(1);
+let str = 'word1 word2 word3';
+let arr = str.split(' ');
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
+}
+let result = arr.join(' ');
 console.log(result);
