58c69e65db9191a89f06c5a5c8e416fcb0f45c02
rotvsmetane <glass.ocean10@gmail.com>
2023-03-09 17:03:49 +0300

    154 урок (Практика на использования изученных методов           Слова в строке)№7

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,4 @@
 "use strict"
 let str = 'Var_Test_Text';
-let arr = str.split('_');
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].slice(0, 1).toLowerCase() + arr[i].slice(1);
-}
-let result = arr.join('');
+let result = str.split('').reverse().join('');
 console.log(result);
