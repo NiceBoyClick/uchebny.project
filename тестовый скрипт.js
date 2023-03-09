b6a17b41423f5d70b6286894f6f1e100199acc32
rotvsmetane <glass.ocean10@gmail.com>
2023-03-09 20:08:28 +0300

    155 урок (Поиск ошибок в коде со стандартными методами)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,11 +1,11 @@
 "use strict"
-let num1 = 12345;
-let arr1 = String(num1).split('');
+let num = 12345;
+let arr = String(num).split('');
 
-let sum1 = 0;
-for (let digit of arr1) {
-    sum1 += +digit;
+let sum = 0;
+for (let digit of arr) {
+    sum += Number(digit);
 }
 
-console.log(sum1);
+console.log(sum)
 
