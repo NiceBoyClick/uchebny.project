e6ced78ebc4b0c595f415996c27b19a7169ba83b
rotvsmetane <glass.ocean10@gmail.com>
2023-03-09 16:59:33 +0300

    154 урок (Практика на использования изученных методов           Слова в строке)№6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,8 @@
 "use strict"
-let str = 'var_test_text';
+let str = 'Var_Test_Text';
 let arr = str.split('_');
 for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
+    arr[i] = arr[i].slice(0, 1).toLowerCase() + arr[i].slice(1);
 }
 let result = arr.join('');
 console.log(result);
