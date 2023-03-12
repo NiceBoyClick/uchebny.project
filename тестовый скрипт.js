383e5f21fe36567f63390c4edb50ecae1e5baf9c
rotvsmetane <glass.ocean10@gmail.com>
2023-03-12 13:21:29 +0300

    162 урок (Последовательный вызов функций)№2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,8 +3,8 @@ function sqrt(num) {
     return Math.sqrt(num);
 }
 
-function round(num) {
-    return num.toFixed(3);
+function sum(num1, num2, num3) {
+    return num1 + num2 + num3;
 }
-let res = round(sqrt(2))
+let res = sum(sqrt(2), sqrt(3), sqrt(4));
 console.log(res);
