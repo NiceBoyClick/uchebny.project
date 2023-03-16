4d67146c253490b7717b7f363a89b64ec86fe5a5
rotvsmetane <glass.ocean10@gmail.com>
2023-03-16 13:35:47 +0300

    168 урок (Флаги в функциях)№1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,5 @@
 "use strict"
-function func(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[j] === arr[i]) {
-                return true;
-            }
-        }
-    }
-    return false;
+function func(a, b) {
+        return a == b;
 }
-let arr = func([2, 4, 6, 8, 8]);
-console.log(arr);
+console.log(func());
