e6f97ab7febc7e379114b5edb4c24d78f5ef7373
rotvsmetane <glass.ocean10@gmail.com>
2023-03-17 13:19:45 +0300

    170 урок (Поиск ошибок в коде с функциями)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,11 +1,12 @@
 "use strict"
-function sum(arr) {
+let arr = func([1, 2, 3, 4, 5]);
+
+function func(arr) {
     let res = 0;
 
     for (let elem of arr) {
         res += elem;
     }
-    return res;
+return res
 }
-
-console.log(sum([1, 2, 3, 4, 5]));
+console.log(arr);
