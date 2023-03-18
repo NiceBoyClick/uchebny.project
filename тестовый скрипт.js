f14278099e298d43558edfc452b85358c1997118
rotvsmetane <glass.ocean10@gmail.com>
2023-03-18 10:40:32 +0300

    170 урок (Поиск ошибок в коде с функциями)№4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,9 @@
 "use strict"
-let arr = func([1, 2, 3, 4, 5]);
-
-function func(arr) {
-    let res = 0;
-
-    for (let elem of arr) {
-        res += elem;
-    }
-return res
+function func1() {
+    return 3;
 }
-console.log(arr);
+function func2() {
+    return 5;
+}
+
+console.log( func1() + func2() );
