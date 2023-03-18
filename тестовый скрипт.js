e07339fd164f1aa50de1deafcea31d7b6cf9b6cb
rotvsmetane <glass.ocean10@gmail.com>
2023-03-18 15:58:17 +0300

    171 урок (Практика на функции)№3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,11 +1,8 @@
 "use strict"
-function findDivisors(num){
-    let arr = [];
-    for (let i = 1; i < num; i++) {
-        if (num % i == 0) {
-            arr.push(i);
-        }
-    }
+let str = showSim('67354dfjh');
+function showSim(str){
+    let arr = str.split('');
     return arr;
 }
-console.log(findDivisors(24));
+
+console.log(str);
