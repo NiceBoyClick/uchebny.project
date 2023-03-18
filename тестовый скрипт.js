55af75b8a474b36aecff34cb7340e504d71f6ec0
rotvsmetane <glass.ocean10@gmail.com>
2023-03-18 16:15:41 +0300

    171 урок (Практика на функции)№4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,8 @@
 "use strict"
-let str = showSim('67354dfjh');
+let str = showSim('ldfjh345');
 function showSim(str){
-    let arr = str.split('').reverse().join('');
-    return arr;
+    let res = str.slice(0, 1).toUpperCase() + str.slice(1);
+    return res;
 }
 
 console.log(str);
