473dc791e681f690fc3101cda2e12fe9821572c8
rotvsmetane <glass.ocean10@gmail.com>
2023-03-18 17:07:25 +0300

    171 урок (Практика на функции)№6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,12 @@
 "use strict"
-let str = showSim('ldfjh345');
+let str = showSim('bullet per second');
 function showSim(str){
-    let res = str.slice(0, 1).toUpperCase() + str.slice(1);
-    return res;
+    let words = str.split(' ');
+    console.log(words);
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].slice(0, 1).toUpperCase() + words[i].slice(1);
+    }
+    let result = words.join(' ');
+    return result;
 }
 console.log(str);
