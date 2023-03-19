627a833b1b49df5d4b79a9d5509c28200f5c5579
rotvsmetane <glass.ocean10@gmail.com>
2023-03-19 19:43:30 +0300

    171 урок (Практика на функции)№7

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,9 @@
 "use strict"
-let str = showSim('bullet per second');
-function showSim(str){
-    let words = str.split(' ');
-    console.log(words);
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i].slice(0, 1).toUpperCase() + words[i].slice(1);
+function addNumber(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+      arr.push(i)
     }
-    let result = words.join(' ');
-    return result;
+    return arr;
 }
-console.log(str);
+console.log(addNumber(8));
