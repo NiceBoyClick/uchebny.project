acc9c96487505f779edfdc25d10dc26dc1362515
rotvsmetane <glass.ocean10@gmail.com>
2023-03-22 10:32:57 +0300

    171 урок (Практика на функции)№11

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,12 +3,12 @@ function  areFriendlyNumbers(num1, num2) {
     let sum1 = 0;
     let sum2 = 0;
 
-    for (let i = 1; i < num1; i++) {
+    for (let i = 1; i <= num1 / 2; i++) {
         if(num1 % i === 0) {
             sum1 += i;
         }
     }
-    for (let i = 1; i < num2; i++) {
+    for (let i = 1; i <= num2 / 2; i++) {
         if(num2 % i === 0) {
             sum2 += i;
         }
@@ -20,4 +20,11 @@ function  areFriendlyNumbers(num1, num2) {
         return false;
     }
 }
-console.log(areFriendlyNumbers(220, 284));
+
+for (let i = 1; i <= 1000; i++) {
+    for (let j = i + 1; j <= 1000; j++) {
+        if (areFriendlyNumbers(i, j)) {
+            console.log(`${i}, ${j}`);
+        }
+    }
+}
