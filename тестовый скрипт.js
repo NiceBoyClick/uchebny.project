5c03eee7437a9ede52dc74f63ff9a1e037894522
rotvsmetane <glass.ocean10@gmail.com>
2023-03-22 10:13:31 +0300

    171 урок (Практика на функции)№10

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,10 +1,23 @@
 "use strict"
-function getPrime(num) {
-    for (let i = 2; i < num; i++) {
-        if(num % i === 0) {
-            return false;
+function  areFriendlyNumbers(num1, num2) {
+    let sum1 = 0;
+    let sum2 = 0;
+
+    for (let i = 1; i < num1; i++) {
+        if(num1 % i === 0) {
+            sum1 += i;
         }
     }
-    return true;
+    for (let i = 1; i < num2; i++) {
+        if(num2 % i === 0) {
+            sum2 += i;
+        }
+    }
+    if (num2 == sum1 && num1 == sum2) {
+
+        return true;
+    } else {
+        return false;
+    }
 }
-console.log(getPrime(20));
+console.log(areFriendlyNumbers(220, 284));
