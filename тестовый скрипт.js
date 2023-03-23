9bf56dab73c9ba87f5445d6e740b8bd35aa10d69
rotvsmetane <glass.ocean10@gmail.com>
2023-03-23 09:54:07 +0300

    171 урок (Практика на функции)№12

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,30 +1,14 @@
 "use strict"
-function  areFriendlyNumbers(num1, num2) {
-    let sum1 = 0;
-    let sum2 = 0;
-
-    for (let i = 1; i <= num1 / 2; i++) {
-        if(num1 % i === 0) {
-            sum1 += i;
+function arePerfectNum(num) {
+let sum = 0;
+    for (let i = 1; i <= num / 2; i++) {
+        if (num % i ===0) {
+            sum += i;
         }
     }
-    for (let i = 1; i <= num2 / 2; i++) {
-        if(num2 % i === 0) {
-            sum2 += i;
-        }
-    }
-    if (num2 == sum1 && num1 == sum2) {
-
+    if (sum === num) {
         return true;
-    } else {
-        return false;
     }
+    return false;
 }
-
-for (let i = 1; i <= 1000; i++) {
-    for (let j = i + 1; j <= 1000; j++) {
-        if (areFriendlyNumbers(i, j)) {
-            console.log(`${i}, ${j}`);
-        }
-    }
-}
+console.log(arePerfectNum(495))
