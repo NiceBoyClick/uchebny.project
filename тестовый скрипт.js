2e1adfd44b4c93de19e7eeebe3f02bd801d6141c
rotvsmetane <glass.ocean10@gmail.com>
2023-03-24 11:42:32 +0300

    171 урок (Практика на функции)№13

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,14 +1,29 @@
 "use strict"
-function arePerfectNum(num) {
-let sum = 0;
-    for (let i = 1; i <= num / 2; i++) {
-        if (num % i ===0) {
-            sum += i;
-        }
-    }
-    if (sum === num) {
-        return true;
-    }
-    return false;
+function isLuckyTicket(ticketNumber) {
+    // Преобразуем номер билета в строку и разбиваем на две половины
+    const ticketString = String(ticketNumber);
+    const firstHalf = ticketString.slice(0, 3);
+    const secondHalf = ticketString.slice(3);
+
+    // Считаем суммы цифр в каждой половине
+    const firstHalfSum = sumDigits(firstHalf);
+    const secondHalfSum = sumDigits(secondHalf);
+
+    // Сравниваем суммы и возвращаем true, если они равны
+    return firstHalfSum === secondHalfSum;
 }
-console.log(arePerfectNum(495))
+
+function sumDigits(str) {
+    // Преобразуем строку в массив символов и считаем сумму цифр
+    return str.split('').reduce((acc, digit) => acc + Number(digit), 0);
+}
+
+// Ищем все счастливые билеты
+const luckyTickets = [];
+for (let i = 0; i < 1000000; i++) {
+    if (isLuckyTicket(i)) {
+        luckyTickets.push(i);
+    }
+}
+
+console.log(luckyTickets);
