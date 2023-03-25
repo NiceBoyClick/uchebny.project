ec76fcc62e16aacaf25580cebafe7e1ed23d3aed
rotvsmetane <glass.ocean10@gmail.com>
2023-03-25 15:29:08 +0300

    6 урок (циклы в TypeScript)№2

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+var arr = [1, 2, 3, 4, 5];
 var res = 0;
-for (var i = 0; i < 100; i++) {
-    res += i;
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var elem = arr_1[_i];
+    res += elem;
 }
 console.log(res);
--- test.ts
+++ test.ts
@@ -1,7 +1,8 @@
-let res = 0;
+let arr: number [] = [1, 2, 3, 4, 5];
+let res: number = 0;
 
-for (let i: number = 0; i < 100; i++) {
-  res += i;
+for (let elem of arr) {
+  res += elem;
 }
 
 console.log(res);
