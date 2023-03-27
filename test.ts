083a9eb96fe324275c03995f0eee7d5f99e920c3
rotvsmetane <glass.ocean10@gmail.com>
2023-03-27 09:39:42 +0300

    7 урок (Функции в TypeScript)№1

--- test.ts
+++ test.ts
@@ -1,8 +1,3 @@
-let arr: number [] = [1, 2, 3, 4, 5];
-let res: number = 0;
-
-for (let elem of arr) {
-  res += elem;
+function sum(x: number, y: number): number {
+  return x + y;
 }
-
-console.log(res);
