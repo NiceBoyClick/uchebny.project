dcdd86a3239a1588a618d2a8856ee93861342970
rotvsmetane <glass.ocean10@gmail.com>
2023-03-28 09:39:25 +0300

    8 урок (Деструктуризация кортежа)№8

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-var date;
-date = [27, 3];
-console.log(date);
+var time = [12, 59, 59];
+var hour = time[0], minute = time[1], second = time[2];
+console.log(hour);
+console.log(minute);
+console.log(second);
