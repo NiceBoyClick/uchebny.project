7761d977b741a380cd091e4cc1c2d9506c8aaf83
rotvsmetane <glass.ocean10@gmail.com>
2023-03-29 10:58:25 +0300

    4 урок (Правила возврата функцией тегов JSX)№2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 function App() {
 
-  return <div>
+  return <>
+  <div>
+    <ul>
+    <li>text1</li>
+    <li>text2</li>
+    <li>text3</li>
+  </ul>
     <ul>
       <li>text1</li>
       <li>text2</li>
       <li>text3</li>
-    </ul>;
-  </div>;
+    </ul>
+  </div>
+  </>;
 }
 
 export default App;
