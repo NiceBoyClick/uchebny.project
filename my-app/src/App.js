fca7f14cf4a0d0264b2672b7657bd977baecf2ab
rotvsmetane <glass.ocean10@gmail.com>
2023-03-29 10:12:44 +0300

    3 урок (Введение в язык JSX в React)№5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 function App() {
   return <div>
-    <ul>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-      <li>unit</li>
-    </ul>
+    <table border={1} width={200}>
+      <tbody>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+      </tr>
+      </tbody>
+    </table>;
   </div>;
 }
 
