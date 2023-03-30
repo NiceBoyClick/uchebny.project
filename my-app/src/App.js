07556deaa9262e7cfd38b21008146e662bac1192
rotvsmetane <glass.ocean10@gmail.com>
2023-03-30 08:53:37 +0300

    5 урок (Вставка значений переменных и констант в JSX)№1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function App() {
-  return <>
-  <input/>
-   <input/>
-   <input/>
-  </>
-    }
+  const str1 = 'text1';
+  const str2 = 'text1';
+
+  return <div>
+    <p>{str1}</p>
+    <p>{str2}</p>
+  </div>;
+}
 
 export default App;
