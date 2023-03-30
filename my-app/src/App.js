40bfdcce6e5fd38f026247897f66a5fd769384e5
rotvsmetane <glass.ocean10@gmail.com>
2023-03-30 09:12:11 +0300

    5 урок (Вставка значений переменных и констант в JSX) №6

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 function App() {
-  const num1 = 3;
-  const num2 = 2;
+  const arr = [1, 2, 3, 4, 5];
 
   return <div>
-    result: {num1 ** num2}
-  </div>;
+    <ul>
+      <li>{arr[0]}</li>
+      <li>{arr[1]}</li>
+      <li>{arr[2]}</li>
+      <li>{arr[3]}</li>
+      <li>{arr[4]}</li>
+    </ul>
+  </div>
 }
 
 export default App;
