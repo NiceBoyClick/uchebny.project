c3b8b4dcf7df889fd9f2e89d3eb7aa453caa26f7
rotvsmetane <glass.ocean10@gmail.com>
2023-04-01 10:06:49 +0300

    11 урок (Навешивание событий в JSX) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function App() {
-  const isAuth = false;
+  function show1() {
+    alert(1);
+  }
+
+  function show2() {
+    alert(2);
+  }
 
   return <div>
-    {!isAuth && <p>пожалуйста, авторизуйтесь</p>}
+    <button onClick={show1}>act1</button>
+    <button onClick={show2}>act2</button>
   </div>;
 }
 
