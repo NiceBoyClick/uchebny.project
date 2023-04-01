8279a111377e150425c647fef8d6fd8e2987ebbd
rotvsmetane <glass.ocean10@gmail.com>
2023-04-01 10:47:53 +0300

    11 урок (Навешивание событий в JSX) №2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
 function App() {
-  function show1() {
-    alert(1);
-  }
-
-  function show2() {
-    alert(2);
+  function showNumber(num) {
+    alert(num);
   }
 
   return <div>
-    <button onClick={show1}>act1</button>
-    <button onClick={show2}>act2</button>
+    <button onClick={() => showNumber(1)}>act1</button>
+    <button onClick={() => showNumber(2)}>act2</button>
+    <button onClick={() => showNumber(3)}>act3</button>
   </div>;
 }
 
