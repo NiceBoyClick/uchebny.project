a6a6e3ebdadd4f99cfbd81b249340bccb27e7431
rotvsmetane <glass.ocean10@gmail.com>
2023-04-10 09:20:38 +0300

    183 урок (Массив с анонимными функциями в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,11 @@
 'use Strict';
 
-let func1 = function() {console.log('!')};
-let func2 = function() {
-  console.log('!');
-};
-function func3() {
-  console.log('!');
-}
+let arr = [
+  function () {return 1},
+  function () {return 2},
+  function () {return 3},
+]
+
+console.log(arr[0]());
+console.log(arr[1]());
+console.log(arr[2]());
