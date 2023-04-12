12496afd731df327ff1d9cf0511e4815aea08fab
rotvsmetane <glass.ocean10@gmail.com>
2023-04-12 10:25:59 +0300

    185 урок (Именованные функции) №3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,17 +1,17 @@
 'use Strict';
 
-function get1() {
+function func1() {
   return 1;
 }
-function get2() {
-  return 2;
+function func2() {
+  return -2;
 }
-function get3() {
-  return 3;
+function func3() {
+  return -3;
 }
 
-test(get1, get2, get3);
+test(func1, func2, func3);
 
 function test(func1, func2, func3) {
-  console.log(func1() + func2() + func3());
+  console.log(func1(), func2(),  func3());
 }
