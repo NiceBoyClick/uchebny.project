84ec0e91f7471ccd5d21c014941b03cac60ff42c
rotvsmetane <glass.ocean10@gmail.com>
2023-04-12 10:55:31 +0300

    185 урок (Параметры передаваемых функций) №5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,17 +1,9 @@
 'use Strict';
 
-let func1 = function() {
-  return 1;
-}
-let func2 = function() {
-  return -2;
-}
-let func3 = function() {
-  return -3;
-}
+test(function(num) {
+  return num * num * num;
+});
 
-test(func1, func2, func3);
-
-function test(func1, func2, func3) {
-  console.log(func1(), func2(),  func3());
+function test(func) {
+  console.log(func(3));
 }
