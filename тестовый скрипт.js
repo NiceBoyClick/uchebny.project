b2d6e8239a12b70aa79acacf397acb49731270b5
rotvsmetane <glass.ocean10@gmail.com>
2023-04-14 14:51:53 +0300

    185 урок (Параметры передаваемых функций) №9

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,10 +1,17 @@
 'use Strict';
 
-let func = function(num1, num2) {
-  return num1 + num2;
+function square(num) {
+  return num * num;
 }
 
-function test(param) {
-  console.log(param(2, 3));
+function cube(num) {
+  return num * num * num;
 }
-test(func);
+
+function test(num, func1, func2) {
+  return func1(num) + func2(num);
+}
+
+let num = 3;
+let result = test(num, square, cube);
+console.log(result);
