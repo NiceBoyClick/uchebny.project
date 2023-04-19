0af3dfaec64443b5528af91c8780a13d4a836905
rotvsmetane <glass.ocean10@gmail.com>
2023-04-19 10:27:44 +0300

    189 урок (Функция, возвращающая функцию в JavaScript) №4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,9 +3,15 @@
 function func(num1) {
   return function (num2) {
     return function (num3) {
-      return num1 + num2 + num3;
+      return function (num4) {
+        return function () {
+          arr.push(num1, num2, num3, num4);
+          return arr;
+        };
+      };
     };
   };
-}
+};
 
-console.log(func(2)(3)(4));
+let arr = [];
+console.log(func(2)(3)(4)(5)());
