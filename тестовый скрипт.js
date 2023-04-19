212b347675682883a7f7990b3de72c4d0bc99101
rotvsmetane <glass.ocean10@gmail.com>
2023-04-19 10:04:58 +0300

    189 урок (Функция, возвращающая функцию в JavaScript) №3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,16 +1,11 @@
 'use Strict';
 
-function func1() {
-  return function () {
-    return function () {
-      return function () {
-        return function () {
-          return '!';
-        }
-      }
-    }
-  }
-};
+function func(num1) {
+  return function (num2) {
+    return function (num3) {
+      return num1 + num2 + num3;
+    };
+  };
+}
 
-
-console.log(func1()()()()());
+console.log(func(2)(3)(4));
