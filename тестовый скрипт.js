ea901cec87dd443660d22f2e65593fa8d1c665df
rotvsmetane <glass.ocean10@gmail.com>
2023-04-20 14:42:48 +0300

    191 урок (Замыкания в JavaScript) №3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -4,12 +4,25 @@ function test() {
   let num = 10;
 
   return function () {
-    console.log(num);
-    num--;
+    if (num >= 0) {
+      console.log(num);
+      num--;
+    } else {
+      console.log('отсчет окончен');
+    }
   }
 }
 
 let func = test();
+func();
+func();
+func();
+func();
+func();
+func();
+func();
+func();
+func();
 func();
 func();
 func();
