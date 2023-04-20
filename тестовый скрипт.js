fceefe6c8c812571aa6041566707a7c5e951b28c
rotvsmetane <glass.ocean10@gmail.com>
2023-04-20 10:47:59 +0300

    191 урок (Замыкания в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,17 +1,15 @@
 'use Strict';
 
-function func(num1) {
-  return function (num2) {
-    return function (num3) {
-      return function (num4) {
-        return function () {
-          arr.push(num1, num2, num3, num4);
-          return arr;
-        };
-      };
-    };
-  };
-};
+function test() {
+  let num = 1;
 
-let arr = [];
-console.log(func(2)(3)(4)(5)());
+  return function () {
+    console.log(num);
+    num++;
+  }
+}
+
+let func = test();
+func();
+func();
+func();
