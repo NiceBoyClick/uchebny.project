78023f534b994dd7eaf764950fc4765a3537879d
rotvsmetane <glass.ocean10@gmail.com>
2023-04-21 10:39:08 +0300

    193 урок (Вызов функции на месте в JavaScript) №12

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,28 +1,9 @@
 'use Strict';
 
-function test() {
-  let num = 10;
-
+(function() {
   return function () {
-    if (num >= 0) {
-      console.log(num);
-      num--;
-    } else {
-      console.log('отсчет окончен');
+    return function (){
+      console.log('!')
     }
   }
-}
-
-let func = test();
-func();
-func();
-func();
-func();
-func();
-func();
-func();
-func();
-func();
-func();
-func();
-func();
+})()()();
