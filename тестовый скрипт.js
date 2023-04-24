8b6c405ec31cc79508c0ba7b819606e0802e8143
rotvsmetane <glass.ocean10@gmail.com>
2023-04-24 10:27:20 +0300

    193 урок (Замыкания и вызов функции на месте) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,11 +3,23 @@
 let func = (function () {
   let num = 1;
   return function () {
-    console.log(num);
-    num++;
+    if (num <= 5) {
+      console.log(num);
+      num++;
+    } else {
+      num = 1
+      console.log(num);
+      num++;
+    }
   }
 })();
 
+func();
+func();
+func();
+func();
+func();
+func();
 func();
 func();
 func();
