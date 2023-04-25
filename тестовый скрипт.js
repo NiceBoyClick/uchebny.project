5ddb0504f70f713cf8dde87233e2f579ac635fb9
rotvsmetane <glass.ocean10@gmail.com>
2023-04-25 10:25:10 +0300

    194 урок (Функции-коллбэки в JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -4,14 +4,14 @@ function each(arr, callback) {
   let result = [];
 
   for (let elem of arr) {
-    result.push( callback(elem) ); // вызываем функцию-коллбэк
+    result.push(callback(elem)); // вызываем функцию-коллбэк
   }
 
   return result;
-}
+};
 
-let result = each([1, 2, 3, 4, 5], function(num) {
-  return num * 2;
+let result = each(['qwe', 'rty', 'uio', 'asd', 'fgh'], function (elem) {
+  return elem.split('').reverse().join('');
 });
 
 console.log(result);
