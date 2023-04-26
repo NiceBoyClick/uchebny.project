5514010ebd63f025e48b6a5912df3de306c42a11
rotvsmetane <glass.ocean10@gmail.com>
2023-04-26 10:35:00 +0300

    195 урок (Стрелочные функции в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,19 +1,16 @@
 'use Strict';
 
-function each(arr, callback) {
+function every(arr, callback) {
   let result = [];
 
   for (let elem of arr) {
-    result.push(callback(elem)); // вызываем функцию-коллбэк
+    result.push(callback(elem));
   }
 
   return result;
 };
 
-function cube(num) {
-  return num ** 3;
-}
 
-let result = each([2, 3, 4, 5, 6], cube);
+let result = every([1, 2, -3, 4, 5],elem => elem > 0);
 
 console.log(result);
