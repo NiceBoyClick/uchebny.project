96e62c55cea38b5b371a8476f4e0191be8abbbaf
rotvsmetane <glass.ocean10@gmail.com>
2023-04-27 10:41:24 +0300

    197 урок (Рекурсия и многомерные структуры в JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,18 @@
 'use Strict';
 
-function func(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] == 'object') {
-      func(obj[key]);
+let array = [];
+
+function func(arr) {
+  for (let elem of arr) {
+    if (typeof elem == 'object') {
+      func(elem);
     } else {
-      console.log(key, obj[key]);
+      array.push(elem);
     }
   }
+  return array;
 }
 
-func({a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}});
+
+func([1, [2, 7, 8], [3, 4, [5, [6, 7]]]]);
+console.log(array);
