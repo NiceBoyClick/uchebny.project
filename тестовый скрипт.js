94b5792eb11d832635edd427c38e01f199b063e7
rotvsmetane <glass.ocean10@gmail.com>
2023-05-01 10:49:23 +0300

    200 урок (Метод filter для фильтрации массива в JavaScript) №6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,5 +1,5 @@
 'use Strict';
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(arr.filter((elem, index) => elem * index < 30));
+let arr = [1, 2, [3, 4], 5, [6, 7]];
+console.log(arr.filter(elem => typeof elem !== "object"));
 
