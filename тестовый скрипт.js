daa014cee6e199f68f5f0bff12e65bfc531a0dee
rotvsmetane <glass.ocean10@gmail.com>
2023-05-01 10:42:22 +0300

    200 урок (Метод filter для фильтрации массива в JavaScript) №5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,5 +1,5 @@
 'use Strict';
 
-let arr = ['adjkfhbgjk', 'ac', 'fvg', 'rthl', 'rifgj', 'eeeeee', 'ufjtovo'];
-console.log(arr.filter(elem => elem.length > 5));
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr.filter((elem, index) => elem * index < 30));
 
