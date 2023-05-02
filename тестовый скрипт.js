e5b3f62431b755511e44e4153663163c2bb92aed
rotvsmetane <glass.ocean10@gmail.com>
2023-05-02 10:29:33 +0300

    203 урок (Оператор spread в JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,7 @@
 'use Strict';
 
 let arr = [1, 2, 3, 4, 5];
-function func(num1, num2, num3, num4, num5) {
-  return num1 + num2 + num3 + num4 + num5;
-}
-console.log(func(...arr));
+let min = Math.min(...arr);
+
+console.log(min);
 
