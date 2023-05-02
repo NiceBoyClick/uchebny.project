cf088149c069cadfdaa90984ef7e49d4d509be71
rotvsmetane <glass.ocean10@gmail.com>
2023-05-02 09:51:01 +0300

    201 урок (Метод every для проверки массива в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,5 +1,5 @@
 'use Strict';
 
-let arr = [-1, 2, -3, 4, 5, -6, -7];
-console.log( arr.filter(elem => elem < 0).length);
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log( arr.every(elem => elem > 0));
 
