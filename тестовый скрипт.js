b162bf1d124a9016f21349c50a5dd2eda87fa531
rotvsmetane <glass.ocean10@gmail.com>
2023-05-03 09:38:42 +0300

    207 урок (Применение операторов rest и spread в JavaScript. Функция, находящая сумму чисел) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,14 @@
 'use Strict';
-
-let arr = [1, 2, 3, 4, 5];
-let min = Math.min(...arr);
-
-console.log(min);
+function func(...nums) {
+  let sum = 0;
+  let mid = 0;
+  for (let elem of nums) {
+    sum += elem;
+    console.log(sum)
+  }
+  mid = sum / 3;
+  return mid.toFixed(1);
+}
+let result = func(1, 2, 3, 4, 5, 6, 7);
+console.log(result);
 
