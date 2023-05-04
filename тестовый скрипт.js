d799089dae1573b822e736f4212204fb19b9e604
rotvsmetane <glass.ocean10@gmail.com>
2023-05-04 10:25:54 +0300

    218 урок (Значения по умолчанию при деструктуризации объектов в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,18 @@
 'use Strict';
 
 let options = {
-  color: 'red',
   width:  400,
   height: 500,
 };
 
-let {color: c, width: w, height: h} = options;
+let {color, width, height} = options;
 
-console.log(c, w, h);
+if (options.color !== undefined) {
+  color = options.color;
+} else {
+  color = 'black';
+}
+
+console.log(color, width, height);
 
 
