95209abf1e44f5d11e0bdff4884dbdca6de65613
rotvsmetane <glass.ocean10@gmail.com>
2023-05-04 09:14:41 +0300

    212 урок (Остаток массива при деструктуризации в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,12 @@
 'use Strict';
 
-let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+let arr = ['John', 'Smit', 'development', 'programmer',
+  2000];
 
-let [,, department, position] = arr;
-console.log(department);
-console.log(position);
+let [name, surname, ...info] = arr;
+console.log(name);
+console.log(surname);
+console.log(info);
+
 
 
