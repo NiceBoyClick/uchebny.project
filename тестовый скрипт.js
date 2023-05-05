91f275e98a9aaa364b713cbf03c867495e33e985
rotvsmetane <glass.ocean10@gmail.com>
2023-05-05 10:22:37 +0300

    221 урок (Деструктуризация параметров функций в JavaScript) №6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,12 @@
 'use Strict';
 
-function func({color, width, height}) {
-  console.log(color);
+function func({width, height, color = 'black'}) {
   console.log(width);
   console.log(height);
+  console.log(color);
 }
 
 func( {color: 'red', width: 400, height: 500} );
-
 
 
 
