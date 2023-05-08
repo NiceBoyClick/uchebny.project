c33ebac185d40811b4093723ceb3b92c204295b2
rotvsmetane <glass.ocean10@gmail.com>
2023-05-08 09:29:19 +0300

    241 урок (Получение DOM элемента в JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,7 @@
 'use Strict';
 
-let paragraph1 = document.querySelector('#elem1');
-console.log(paragraph1);
-
-let paragraph2 = document.querySelector('#elem2');
-console.log(paragraph2);
-
-let paragraph3 = document.querySelector('#elem3');
-console.log(paragraph3);
+let elem = document.querySelector('#block p');
+console.log(elem);
 
 
 
