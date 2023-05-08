de688c5dd76671f1d1dbce3f17dfa0597a4537fe
rotvsmetane <glass.ocean10@gmail.com>
2023-05-08 11:12:22 +0300

    243 урок (Именованные обработчики событий в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,16 +1,15 @@
 'use Strict';
 
 let button1 = document.querySelector('#button1');
-button1.addEventListener('click', function() {
-  alert(1);
-});
+button1.addEventListener('click', func1);
 
 let button2 = document.querySelector('#button2');
-button2.addEventListener('click', function() {
-  alert(2);
-});
+button2.addEventListener('click', func2);
 
-let button3 = document.querySelector('#button3');
-button3.addEventListener('click', function() {
-  alert(3);
-});
+function func1() {
+  console.log(1);
+}
+
+function func2() {
+  console.log(2);
+}
