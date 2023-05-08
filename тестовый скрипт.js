8a03d608324bf7b8e132a1de42289342d881a34c
rotvsmetane <glass.ocean10@gmail.com>
2023-05-08 10:53:00 +0300

    242 урок (Получение DOM элемента в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,7 +1,16 @@
 'use Strict';
 
-let elem = document.querySelector('.www');
-console.log(elem);
+let button1 = document.querySelector('#button1');
+button1.addEventListener('click', function() {
+  alert(1);
+});
 
+let button2 = document.querySelector('#button2');
+button2.addEventListener('click', function() {
+  alert(2);
+});
 
-
+let button3 = document.querySelector('#button3');
+button3.addEventListener('click', function() {
+  alert(3);
+});
