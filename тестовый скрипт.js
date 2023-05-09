184374721ab467cb027b766b8f3c494a6787f0fe
rotvsmetane <glass.ocean10@gmail.com>
2023-05-09 10:29:56 +0300

    247 урок (Работа с текстом элементов на JavaScript) №3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,10 +1,12 @@
 'use Strict';
 
 let button = document.querySelector('#button');
-let elem = document.querySelector('#elem');
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+
 button.addEventListener('click', func1);
 
 
 function func1() {
-  elem.textContent = '!!!';
+  console.log(Number(elem1.textContent) + Number(elem2.textContent));
 }
