3d4f5003709b649f91a59ddaf17a1dc59bb58975
rotvsmetane <glass.ocean10@gmail.com>
2023-05-09 09:44:55 +0300

    245 урок (Один обработчик ко многим элементам в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,17 +1,19 @@
 'use Strict';
 
-let Paragraph1 = document.querySelector('#elem1');
-let Paragraph2 = document.querySelector('#elem2');
-let Paragraph3 = document.querySelector('#elem3');
-let Paragraph4 = document.querySelector('#elem4');
-let Paragraph5 = document.querySelector('#elem5');
+let paragraph = document.querySelector('#elem');
 
-Paragraph1.addEventListener('click', func);
-Paragraph2.addEventListener('click', func);
-Paragraph3.addEventListener('click', func);
-Paragraph4.addEventListener('click', func);
-Paragraph5.addEventListener('click', func);
+paragraph.addEventListener('click', func1);
+paragraph.addEventListener('click', func2);
+paragraph.addEventListener('click', func3);
 
-function func() {
-  console.log('message');
+function func1() {
+  console.log('1');
+}
+
+function func2() {
+  console.log('2');
+}
+
+function func3() {
+  console.log('3');
 }
