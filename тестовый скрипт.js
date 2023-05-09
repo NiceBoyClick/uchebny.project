1a7c3e91eb88b0dbc0328f4586a2a1ae1bcd35d8
rotvsmetane <glass.ocean10@gmail.com>
2023-05-09 09:50:32 +0300

    246 урок (Обработчики разных событий в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,19 +1,10 @@
 'use Strict';
 
-let paragraph = document.querySelector('#elem');
+let button = document.querySelector('#button');
 
-paragraph.addEventListener('click', func1);
-paragraph.addEventListener('click', func2);
-paragraph.addEventListener('click', func3);
+button.addEventListener('dblclick', func1);
+
 
 function func1() {
-  console.log('1');
-}
-
-function func2() {
-  console.log('2');
-}
-
-function func3() {
-  console.log('3');
+  console.log('харош!');
 }
