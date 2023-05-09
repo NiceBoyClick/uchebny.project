1f56a875d51e2eb517a31e673f9f5d92cc7aa5f1
rotvsmetane <glass.ocean10@gmail.com>
2023-05-09 10:06:36 +0300

    247 урок (Работа с текстом элементов на JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -2,12 +2,9 @@
 
 let button = document.querySelector('#button');
 let elem = document.querySelector('#elem');
-
-
 button.addEventListener('click', func1);
 
+
 function func1() {
-  console.log(elem.textContent);
+  elem.textContent = '!!!';
 }
-
-
