06971e4c651842c3f4085a350e2fcd084688638a
rotvsmetane <glass.ocean10@gmail.com>
2023-05-09 10:44:47 +0300

    247 урок (Работа с текстом элементов на JavaScript) №4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,10 +3,12 @@
 let button = document.querySelector('#button');
 let elem1 = document.querySelector('#elem1');
 let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+let division = document.querySelector('#division');
 
 button.addEventListener('click', func1);
 
 
 function func1() {
-  console.log(Number(elem1.textContent) + Number(elem2.textContent));
+ division.textContent = Number(elem1.textContent) + Number(elem2.textContent) + Number(elem3.textContent)
 }
