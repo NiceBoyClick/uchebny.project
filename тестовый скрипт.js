74e9c293fd327b7298361411547e4948d462512b
rotvsmetane <glass.ocean10@gmail.com>
2023-05-10 10:51:06 +0300

    249 урок (Атрибуты тегов как свойства в JavaScript) №4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,11 @@
 'use Strict';
 
-let elem = document.querySelector('#elem');
 let button = document.querySelector('#button');
 let link = document.querySelector('#link');
 
 button.addEventListener('click', func);
 
 function func() {
- elem.textContent += link.href;
+ link.textContent += `(${link.href})`;
 };
 
