5689582a0253972a2eb3a3b36fa185319e53477c
rotvsmetane <glass.ocean10@gmail.com>
2023-05-11 11:14:36 +0300

    250 урок (Работа с текстовыми полями в JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,13 @@
 'use Strict';
 
 let button = document.querySelector('#button');
-let image = document.querySelector('#image');
+/*let image = document.querySelector('#image');*/
 let input = document.querySelector('#elem');
+let paragraph = document.querySelector('#paragraph');
 
 button.addEventListener('click', func1);
 
 function func1() {
- input.value = "введите текст";
+ paragraph.textContent += input.value;
 };
 
