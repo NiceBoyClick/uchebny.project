e892e041dd909c84820a8f3dd105005b51f78a03
rotvsmetane <glass.ocean10@gmail.com>
2023-05-12 10:06:24 +0300

    250 урок (Работа с текстовыми полями в JavaScript) №5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,13 +3,16 @@
 let button = document.querySelector('#button');
 let input1 = document.querySelector('#elem1');
 let input2 = document.querySelector('#elem2');
+let input3 = document.querySelector('#elem3');
+let input4 = document.querySelector('#elem4');
+let input5 = document.querySelector('#elem5');
+let paragraph = document.querySelector('#paragraph');
 
 button.addEventListener('click', func1);
 
 function func1() {
- let val1 = elem1.value;
- let val2 = elem2.value;
- elem1.value = val2;
- elem2.value = val1;
+ let result = (+elem1.value + +elem2.value + +elem3.value + +elem4.value + +elem5.value) / 5;
+ console.log(result);
+ paragraph.textContent = result;
 };
 
