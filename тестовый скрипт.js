155b16e6ce903e5144bcc133419ed99129b292f5
rotvsmetane <glass.ocean10@gmail.com>
2023-05-16 07:57:06 +0300

    253 урок (Цепочки методов и свойств в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,3 @@
 'use Strict';
 
-let input = document.querySelector('#elem');
-let button = document.querySelector('#button');
-
-button.addEventListener('click', func);
-
-function func(num) {
- let arr = [];
- arr.push(elem.className.split(' '));
- console.log(arr);
-};
+ console.log(document.querySelector('#image').src);
