47b70b98ddc2b40677e93d11e3242032add4bb75
rotvsmetane <glass.ocean10@gmail.com>
2023-05-16 08:57:49 +0300

    255 урок (Преимущество this в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,18 @@
 'use Strict';
 
-/*let input = document.querySelector('#elem');*/
-let button = document.querySelector('#button');
 
-button.addEventListener('click', function() {
-  this.textContent ++;
-});
+let par1 = document.querySelector('#par1');
+let par2 = document.querySelector('#par2');
+let par3 = document.querySelector('#par3');
+let par4 = document.querySelector('#par4');
+let par5 = document.querySelector('#par5');
+
+par1.addEventListener('click', func);
+par2.addEventListener('click', func);
+par3.addEventListener('click', func);
+par4.addEventListener('click', func);
+par5.addEventListener('click', func);
+
+function func() {
+  this.textContent += '!';
+}
