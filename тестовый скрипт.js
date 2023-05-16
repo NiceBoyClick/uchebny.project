e94e310eee41e8b312cf43b507a10800a24b9910
rotvsmetane <glass.ocean10@gmail.com>
2023-05-16 08:47:07 +0300

    254 урок (Объект this в JavaScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,10 +1,8 @@
 'use Strict';
 
-let input = document.querySelector('#elem');
+/*let input = document.querySelector('#elem');*/
+let button = document.querySelector('#button');
 
-elem.addEventListener('focus', function() {
-  this.value = 1;
-});
-elem.addEventListener('blur', function() {
-  this.value = 2;
+button.addEventListener('click', function() {
+  this.textContent ++;
 });
