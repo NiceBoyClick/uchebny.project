3272160eb8d28c24123ae49151db2b741475aa59
rotvsmetane <glass.ocean10@gmail.com>
2023-05-19 10:37:02 +0300

    364 урок (Поиск ошибок в коде с DOM в JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,17 +1,6 @@
 'use Strict';
 
-let elems = document.querySelectorAll('li');
-let sum = 0;
-
-for (let elem of elems) {
-  let arr  = [...elem.textContent];
-  let res = arr.reduce(function(sum, elem) {
-    return sum + +elem;
-  }, 0);
-  if(res == 6){
-    console.log(elem.textContent);
-    sum += +elem.textContent;
-  }
+let elems = document.querySelectorAll('p');
+for (const elem of elems) {
+  elem.textContent += '!';
 }
-console.log(sum);
-
