324863903205cf10f37d277b49dafe05679e0610
rotvsmetane <glass.ocean10@gmail.com>
2023-05-22 09:46:46 +0300

    364 урок (Поиск ошибок в коде с DOM в JavaScript) №5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,14 +1,10 @@
 'use Strict';
 
-let button = document.querySelector('button');
 let elems = document.querySelectorAll('p');
-let sum = 0;
 
-button.addEventListener('click', function () {
-  for (let elem of elems) {
-    sum += +elem.textContent;
-  };
-    console.log(sum);
-});
-
+for (let elem of elems) {
+  elem.addEventListener('click', () => {
+    elem.textContent += '!';
+  });
+}
 
