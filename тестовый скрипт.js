3619129bdc7d1135e7ce2d02185ca830372c5723
rotvsmetane <glass.ocean10@gmail.com>
2023-05-23 09:11:06 +0300

    363 урок (Поиск ошибок в коде с DOM в JavaScript) №10

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,11 @@
 'use Strict';
 
 let btn  = document.querySelector('#btn');
+let res  = document.querySelector('#res');
 let inp1 = document.querySelector('#inp1');
 let inp2 = document.querySelector('#inp2');
-let inp3 = document.querySelector('#inp3');
 
 btn.addEventListener('click', function() {
-  inp3.value = +inp1.value + +inp2.value;
+  res.textContent = +inp1.value + +inp2.value;
 });
-
 
