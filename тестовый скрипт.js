3e21c99bb24696674814d8a93056203fb632626e
rotvsmetane <glass.ocean10@gmail.com>
2023-05-23 11:01:49 +0300

    363 урок (Поиск ошибок в коде с DOM в JavaScript) №12

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,14 +3,20 @@
 let inputs = document.querySelectorAll('input');
 let button = document.querySelector('#btn');
 
+let texts = [
+  'text1',
+  'text2',
+  'text3',
+];
+
 button.addEventListener('click', function() {
-  for (let input of inputs) {
-    if (input.value == input.dataset.text) {
+  inputs.forEach(function(input, index) {
+    if (input.value === texts[index]) {
       input.classList.add('right');
     } else {
       input.classList.add('wrong');
     }
     console.log(input.classList);
-  }
+  });
 });
 
