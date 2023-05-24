5ad241d23ad13068e2e977e158607df4cf117b37
rotvsmetane <glass.ocean10@gmail.com>
2023-05-24 09:40:26 +0300

    363 урок (Поиск ошибок в коде с DOM в JavaScript) №13

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,22 +1,12 @@
 'use Strict';
 
 let inputs = document.querySelectorAll('input');
-let button = document.querySelector('#btn');
+let btn = document.querySelector('#btn');
+let sum = 0;
 
-let texts = [
-  'text1',
-  'text2',
-  'text3',
-];
-
-button.addEventListener('click', function() {
-  inputs.forEach(function(input, index) {
-    if (input.value === texts[index]) {
-      input.classList.add('right');
-    } else {
-      input.classList.add('wrong');
-    }
-    console.log(input.classList);
-  });
+btn.addEventListener('click', function() {
+  for (let elem of inputs) {
+    sum += Number(elem.value);
+  }
+  console.log(sum);
 });
-
