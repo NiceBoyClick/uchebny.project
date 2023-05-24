6a63270f81d746a8a5de586613e9a3e679b0675c
rotvsmetane <glass.ocean10@gmail.com>
2023-05-24 10:02:52 +0300

    363 урок (Поиск ошибок в коде с DOM в JavaScript) №15

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,14 @@
 'use Strict';
 
-let btn  = document.querySelector('#btn');
-let inp1 = document.querySelector('#inp1');
-let inp2 = document.querySelector('#inp2');
-let inp3 = document.querySelector('#inp3');
-let sum  = 0;
+let inp = document.querySelector('#inp');
 
-btn.addEventListener('click', function() {
-  sum = (+inp1.value + +inp2.value);
-  inp3.value = sum;
+
+inp.addEventListener('blur', function() {
+  let sum = 0;
+  let digits = inp.value.split('');
+  console.log('digits:', digits);
+  for (let digit of digits) {
+    sum += +digit;
+  }
+  console.log('sum:', sum);
 });
