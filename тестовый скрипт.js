87e644e2066c55e43a54f7ab28ae55778ce45a5f
rotvsmetane <glass.ocean10@gmail.com>
2023-05-24 11:09:17 +0300

    364 урок (Отработка изученного материала на работу с DOM) №4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -5,10 +5,14 @@ let inp = document.querySelector('input');
 
 inp.addEventListener('blur', function() {
   let sum = 0;
-  let digits = inp.value.split('');
+  let mid = 0;
+  let digits = inp.value.split(',');
   console.log('digits:', digits);
   for (let digit of digits) {
     sum += +digit;
   }
+  mid = sum / digits.length;
   console.log('sum:', sum);
+  console.log('digits length:', digits.length);
+  console.log('mid:', mid);
 });
