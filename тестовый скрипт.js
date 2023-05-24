4df2010f4fe53e32e8225a39e5cdf5c64561d5df
rotvsmetane <glass.ocean10@gmail.com>
2023-05-24 10:36:44 +0300

    364 урок (Отработка изученного материала на работу с DOM) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,9 +1,15 @@
 'use Strict';
 
-let inp = document.querySelector('#inp');
+let inputs = document.querySelectorAll('input');
 let par = document.querySelector('p');
+let but = document.querySelector('button');
 
 
-inp.addEventListener('blur', function() {
-  par.textContent += inp.value;z``
+but.addEventListener('click', function() {
+  let sum = 0;
+  for (let input of inputs) {
+    console.log(input.value);
+    sum += +input.value;
+    par.textContent = sum;
+  }
 });
