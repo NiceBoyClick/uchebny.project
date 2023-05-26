a3c8330718e1a35ff9f5d58941821a02185374b3
rotvsmetane <glass.ocean10@gmail.com>
2023-05-26 10:53:12 +0300

    364 урок (Отработка изученного материала на работу с DOM) №6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,13 +1,11 @@
 'use Strict';
 
 let input = document.querySelector('.ФИО');
-let inputs = document.querySelectorAll('.inp');
-
 
 input.addEventListener('blur', function () {
   let names = input.value.split(' ');
-  console.log('name:', names);
-  inputs.forEach(function (input, index) {
-    input.value = names[index] || '';
+  names.forEach(function (elem,index, array) {
+   array[index] = elem.slice(0, 1).toUpperCase() + elem.slice(1);
   });
+  input.value = names.join(' ');
 });
