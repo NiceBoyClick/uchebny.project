c7b33aaf2a0d9179125be74904545231b3c95a77
rotvsmetane <glass.ocean10@gmail.com>
2023-06-15 10:13:11 +0300

    446 урок (Многократное нажатие на кнопку для запуска таймера на JavaScript) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -2,10 +2,12 @@
 
 let start = document.querySelector('#start');
 
-start.addEventListener('click', function () {
+start.addEventListener('click', function func() {
   let i = 100;
 
   setInterval(function () {
     console.log(i--);
   }, 1000);
+
+  this.removeEventListener('click', func);
 });
