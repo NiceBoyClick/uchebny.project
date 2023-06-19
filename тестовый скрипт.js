5417fbd3bb3eb4dbde1f9b89cb7ca5fa1952647b
Константин <glass.ocean10@gmail.com>
2023-06-19 10:26:11 +0300

    448 урок (Практика на таймеры и работу с DOM в JavaScript) №3

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -4,5 +4,7 @@ let elem = document.querySelector('#elem');
 elem.value = 10;
 
 setInterval(function() {
-  elem.value = Number(elem.value) - 1;
+  if (elem.value > 0){
+    elem.value = Number(elem.value) - 1;
+  }
 }, 1000);
