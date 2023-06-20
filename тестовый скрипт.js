930768057cebfdee6b5c7e8f37a583a0163cc2fe
Константин <glass.ocean10@gmail.com>
2023-06-20 10:14:56 +0300

    450 урок (Практика на таймеры и работу с DOM в JavaScript) №6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,14 +1,16 @@
 'use Strict';
 
-let input = document.querySelector('#elem');
 let p = document.querySelector('p');
-let button = document.querySelector('button');
+let button1 = document.querySelector('#start');
+let button2 = document.querySelector('#stop');
+let timerId;
 
-button.addEventListener('click', function() {
-  p.textContent = input.value
-  setInterval(function() {
-    if (p.textContent > 0) {
-    p.textContent--;
-  };
+button1.addEventListener('click', function() {
+  timerId = setInterval(function() {
+    p.textContent++;
   }, 1000);
+});
+
+button2.addEventListener('click', function() {
+  clearInterval(timerId);
 });
