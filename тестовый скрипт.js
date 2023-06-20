33ada8bbc83c60a541243ce84b1d0ac33c6bc23e
Константин <glass.ocean10@gmail.com>
2023-06-20 10:09:00 +0300

    450 урок (Практика на таймеры и работу с DOM в JavaScript) №5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -2,8 +2,9 @@
 
 let input = document.querySelector('#elem');
 let p = document.querySelector('p');
+let button = document.querySelector('button');
 
-input.addEventListener('blur', function() {
+button.addEventListener('click', function() {
   p.textContent = input.value
   setInterval(function() {
     if (p.textContent > 0) {
