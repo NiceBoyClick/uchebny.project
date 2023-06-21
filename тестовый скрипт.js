b3f85461580b22db052296d4018d9c724ebdfefc
Константин <glass.ocean10@gmail.com>
2023-06-21 11:06:11 +0300

    10 урок (Тип any в TypeScript) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -3,7 +3,7 @@
 let par = document.querySelector('p');
 let isRed = true;
 
-setInterval(function() {
- par.classList.add('red')
- par.classList.toggle('green')
-  }, 1000);
+// setInterval(function() {
+//  par.classList.add('red')
+//  par.classList.toggle('green')
+//   }, 1000);
