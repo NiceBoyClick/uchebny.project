7395340168f05d2c88bd196b9ab7954a3ecd4b56
Константин <glass.ocean10@gmail.com>
2023-06-23 11:47:23 +0300

    2 урок (Единицы измерения в атрибуте style в JavaScript) №5

--- Advanced.js
+++ Advanced.js
@@ -4,8 +4,15 @@ let button = document.querySelector('#but');
 let elem = document.querySelector('#elem');
 
 button.addEventListener('click', function() {
-    elem.style.width = parseInt(elem.style.width) + 50 + 'px';
-    elem.style.height = parseInt(elem.style.height) + 50 + 'px';
+    let widthPx = elem.style.width;
+    let widthNm = parseInt(widthPx)
+    let num = widthNm / 100 * 10
+    elem.style.width = (widthNm + num) + 'px';
+
+    let heightPx = elem.style.height;
+    let heightNm = parseInt(heightPx)
+    let num2 = heightNm / 100 * 10
+    elem.style.height = (heightNm + num2) + 'px';
     console.log(elem.style.width);
     console.log(elem.style.height);
 });
