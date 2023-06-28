66b9310db2c416db56d5cb3e639ff008543d2a6e
Константин <glass.ocean10@gmail.com>
2023-06-28 10:45:37 +0300

    learn javaScript(Операторы сравнения) №1

--- Advanced.js
+++ Advanced.js
@@ -1,6 +1,9 @@
 'use Strict';
 
-let a = prompt("Первое число?", '1');
-let b = prompt("Второе число?", '2');
-
-alert(+a + +b);
+// 5 > 4 = true
+// "ананас" > "яблоко" = false
+// "2" > "12" = true = false
+// undefined == null = true
+// undefined === null = false
+// null == "\n0\n" = false
+// null === +"\n0\n" = false
