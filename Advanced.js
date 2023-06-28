2de4d6dc6ffd2a143d85973815eb6edad1c524bf
Константин <glass.ocean10@gmail.com>
2023-06-28 11:17:45 +0300

    learn javaScript(Условное ветвление: if, '?') №4

--- Advanced.js
+++ Advanced.js
@@ -1,10 +1,4 @@
 'use Strict';
 
-let num = prompt('введите число', '');
-if (num > 0) {
-    alert(1);
-} else if (num < 0) {
-    alert(-1);
-} else {
-    alert(0);
-}
+let result = (1 + 2 < 4) ?  'Мало' :  'Много';
+alert(result);
