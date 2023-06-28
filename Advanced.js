0b56a54d40a988e85f9aaabf954e3290fd3a8168
Константин <glass.ocean10@gmail.com>
2023-06-28 11:03:16 +0300

    learn javaScript(Условное jветвление: if, '?') №2

--- Advanced.js
+++ Advanced.js
@@ -1,9 +1,8 @@
 'use Strict';
 
-// 5 > 4 = true
-// "ананас" > "яблоко" = false
-// "2" > "12" = true = false
-// undefined == null = true
-// undefined === null = false
-// null == "\n0\n" = false
-// null === +"\n0\n" = false
+let official = prompt('Какое «официальное» название JavaScript?', '');
+if (official == 'ECMAScript') {
+    alert('верно!');
+} else {
+    alert('Не знаете? ECMAScript!');
+}
