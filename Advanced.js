394b314ec340f634bb4c0307c126de61d340ec57
Константин <glass.ocean10@gmail.com>
2023-07-03 11:57:59 +0300

    learn javaScript(Циклы while и for) №4

--- Advanced.js
+++ Advanced.js
@@ -1,17 +1,5 @@
 'use Strict';
-
-let ask = prompt('кто там?', '');
-
-if (ask === 'админ') {
-    password = prompt('пароль?', '');
-} else if (ask === null) {
-    alert('отменено');
-} else {
-    alert('я вас не знаю')
-} if (password === 'я главный') {
-    alert('здравствуйте!');
-} else if (password === null) {
-    alert('отменено');
-} else {
-    alert('неверный пароль');
+for (let i = 2; i <= 10 ; i++) {
+    if (i % 2 !== 0) continue;
+    console.log(i);
 }
