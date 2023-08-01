aec678ff4cd82c0828833bc1e024bc6ff8baa191
Константин <glass.ocean10@gmail.com>
2023-08-01 10:30:35 +0300

    learn javaScript(Конструкция "switch") №2

--- Advanced.js
+++ Advanced.js
@@ -1,11 +1,17 @@
 'use Strict';
-if (browser === 'Edge') {
-    alert("You've got the Edge!");
-} else if (browser === 'Chrome'
-    || browser === 'Firefox'
-    || browser === 'Safari'
-    || browser === 'Opera') {
-    alert( 'Okay we support these browsers too' );
-} else {
-    alert( 'We hope that this page looks ok!' );
+
+const number = +prompt('Введите число между 0 и 3', '');
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0')
+        break;
+
+    case 1:
+        alert('Вы ввели число 1')
+        break;
+
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3')
+        break;
 }
