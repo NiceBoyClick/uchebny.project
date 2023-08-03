8035abb1749d7f9f95b2845930cfb4115569e117
Константин <glass.ocean10@gmail.com>
2023-08-03 19:05:59 +0300

    learn javaScript(Функции) №5

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -1,9 +1,20 @@
 'use Strict';
 
-function min(a,b) {
-return  a < b ? a : b;
+function pow(x, n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
 }
 
-let res = min(1, 1);
+let x = prompt("x?", '');
+let n = prompt("n?", '');
 
-console.log(res);
+if (n < 1) {
+    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+    alert( pow(x, n) );
+}
