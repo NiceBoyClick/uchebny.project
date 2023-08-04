7c3e26b35cba214b07d2ba00ffae6bea2abbb8d4
Константин <glass.ocean10@gmail.com>
2023-08-04 11:25:14 +0300

    learn javaScript(Стрелочные функции, основы) №1

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -1,20 +1,11 @@
 'use Strict';
 
-function pow(x, n) {
-    let result = x;
-
-    for (let i = 1; i < n; i++) {
-        result *= x;
-    }
-
-    return result;
+function ask(question, yes, no) {
+    confirm(question) ? yes() : no();
 }
 
-let x = prompt("x?", '');
-let n = prompt("n?", '');
-
-if (n < 1) {
-    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
-} else {
-    alert( pow(x, n) );
-}
+ask(
+    "Вы согласны?",
+    () => alert("Вы согласились."),
+    () => alert("Вы отменили выполнение.")
+);
