cd0b41268c430b867dccb66b3c9025eca4494e70
Костя <glass.ocean10@gmail.com>
2023-08-08 11:10:19 +0300

    задачник(уровень1.1) №6

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,12 @@
 'use Strict';
 
-let first = "первое";
-let second = "полдник";
+function showLastLetter(word) {
+    let arr = word.split('');
+    if (arr[arr.length - 1] == 'ь') {
+        return (arr[arr.length - 2])
+    } else {
+        return (arr[arr.length - 1]);
+    }
+}
 
-(first[0] === second[0]) ?
-console.log("совпадают") :
-console.log("несовпадают");
+console.log(showLastLetter("слово"));
