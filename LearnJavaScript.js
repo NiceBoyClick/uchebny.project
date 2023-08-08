c5964b98aebcb182b8d558d394f42806f30050a7
Костя <glass.ocean10@gmail.com>
2023-08-08 10:05:31 +0300

    LearnJavascript(Советы по стилю кода) №1

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -1,11 +1,21 @@
 'use Strict';
 
-// function ask(question, yes, no) {
-//     confirm(question) ? yes() : no();
-// }
-//
-// ask(
-//     "Вы согласны?",
-//     () => alert("Вы согласились."),
-//     () => alert("Вы отменили выполнение.")
-// );
+function pow(x, n) {
+    let result = 1;
+
+    for(let i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+let x = prompt("x?",""),
+    n = prompt("n?","");
+
+if (n <= 0) {
+    alert(`Степень ${n} не поддерживается, 
+    введите целую степень, большую 0`);
+} else {
+    alert( pow(x,n) );
+}
