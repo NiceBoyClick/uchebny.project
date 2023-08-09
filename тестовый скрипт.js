840f6f40e94aac6dd6977d1514c3b923a4ace3c9
Костя <glass.ocean10@gmail.com>
2023-08-09 11:14:51 +0300

    задачник(уровень1.2) №2

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,11 @@
 'use Strict';
 
-function showLastLetter(word) {
-    let arr = word.split('');
-    if (arr[arr.length - 1] == 'ь') {
-        return (arr[arr.length - 2])
-    } else {
-        return (arr[arr.length - 1]);
-    }
+function showLastLetter(number) {
+    let arr = number.split('');
+
+    return (arr[arr.length - 1]);
 }
 
-console.log(showLastLetter("слово"));
+let result = showLastLetter("48");
+
+console.log(+result);
