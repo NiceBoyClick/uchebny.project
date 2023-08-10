cf2030aa20226b12b43b0dab6b156451c59cde01
Константин <glass.ocean10@gmail.com>
2023-08-10 21:23:00 +0300

    задачник(уровень1.2) №5

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,15 +1,12 @@
 'use Strict';
 
-function func(number) {
-    let arr = number.split('');
-    let quantity = 0;
-
-    for (let i = 0; i <= arr.length; i++) {
-        quantity = i;
-    }
-    return (quantity);
+function func(num1, num2) {
+    let prime = num1.split('');
+    let second = num2.split('');
+    if (prime[0] === second[0]) {
+        return console.log("совпадают");
+    };
+    return console.log('не совпадают');
 }
 
-let result = func('4456');
-
-console.log(result);
+let result = func('2', '5');
