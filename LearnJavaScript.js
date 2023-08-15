d201c5fe7b7e665134934b1397f794f431817956
Константин <glass.ocean10@gmail.com>
2023-08-15 13:18:03 +0300

    learn.javascript(Объекты) #1

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -1,21 +1,7 @@
-// 'use Strict';
-//
-// function pow(x, n) {
-//     let result = 1;
-//
-//     for(let i = 0; i < n; i++) {
-//         result *= x;
-//     }
-//
-//     return result;
-// }
-//
-// let x = prompt("x?",""),
-//     n = prompt("n?","");
-//
-// if (n <= 0) {
-//     alert(`Степень ${n} не поддерживается,
-//     введите целую степень, большую 0`);
-// } else {
-//     alert( pow(x,n) );
-// }
+'use Strict';
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
