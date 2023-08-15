9a699c9a9153b2d4e7be177f341d0604e33178aa
Константин <glass.ocean10@gmail.com>
2023-08-15 13:36:09 +0300

    задачник(уровень1.5) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,12 +1,11 @@
 'use Strict';
 
 function func() {
+    let sum = 0;
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0) {
-            console.log(i)
-        }
-        continue;
+        sum += i;
     }
+    console.log(sum);
 }
 func();
 
