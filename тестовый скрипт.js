de81e52bcee178e3e726ec30c87a07697373546a
Константин <glass.ocean10@gmail.com>
2023-08-15 13:29:08 +0300

    задачник(уровень1.4) №4

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,8 +1,11 @@
 'use Strict';
 
 function func() {
-    for (let i = 100; i >= 1; i--) {
-        console.log(i);
+    for (let i = 1; i <= 100; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+        continue;
     }
 }
 func();
