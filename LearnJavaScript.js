46546cad6d89de9be00b8faca10ccba98fbc91c5
Константин <glass.ocean10@gmail.com>
2023-08-16 17:46:48 +0300

    learn.javascript(Объекты) #6

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -1,17 +1,18 @@
 'use Strict';
 
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-}
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
 
-function func() {
-    let sum = 0;
-    for (let key in salaries) {
-        sum += salaries[key]
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'number') {
+            obj[key] *= 2;
+        }
     }
-    return sum;
+    return obj;
 }
 
-console.log(func());
+console.log(multiplyNumeric(menu));
