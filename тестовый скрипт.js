43a9257fca53dc64deae0041f3e9fcc918ca516a
Константин <glass.ocean10@gmail.com>
2023-09-04 19:46:38 +0300

    задачник(уровень1.10) №1

--- тестовый скрипт.js
+++ тестовый скрипт.js
@@ -1,21 +1,8 @@
 'use Strict';
 
-let arr = [
-    100,
-    200,
-    300,
-    400,
-    500,
-];
-
-function func() {
-    let arr2 = [];
-    for (let elem of arr) {
-        elem += elem / 100 * 10;
-        arr2.push(elem);
-    }
-    return arr2;
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(func(arr));
+console.log(getRandomInt(1, 100));
 
