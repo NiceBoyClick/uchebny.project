c22a328e996b7046b2055bad5d2cdf8e030685ae
Константин <glass.ocean10@gmail.com>
2023-09-05 13:00:11 +0300

    урок 53(Символы ключи объектов в JavaScript) №1

---
+++ Учебник.js
@@ -0,0 +1,5 @@
+'use Strict';
+
+let obj = {a: 1, b: 2, c: 3};
+let sym = Symbol();
+obj[sym] = 'text';
--- тестовый скрипт.js
+++
@@ -1,8 +0,0 @@
-'use Strict';
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-console.log(getRandomInt(1, 100));
-
