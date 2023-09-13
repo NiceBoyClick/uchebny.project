50c79b4c3c7945bf474a20d83ed84a6e7dcecabb
Константин <glass.ocean10@gmail.com>
2023-09-13 11:01:51 +0300

    Задачник(уровень 1.10) №3

--- Задачник.js
+++ Задачник.js
@@ -1,12 +1,9 @@
 'use Strict';
 
-let num = 12345;
-
-function func() {
-    let arr = String(num).split('').reverse();
-    for (elem of arr) {
-        console.log(+elem);
-    }
-}
-
-func(num);
+let arr = [1, 2, 3, 4, 5, 6];
+let sub = arr.slice(0, 2);
+let sub2 = arr.slice(2, 4);
+let sub3 = arr.slice(4, 6);
+console.log(sub);
+console.log(sub2);
+console.log(sub3);
