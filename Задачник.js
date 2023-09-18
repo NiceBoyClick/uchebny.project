5e014a9ea7fcff7e31c4bc82b377d358f8790052
Константин <glass.ocean10@gmail.com>
2023-09-18 11:18:30 +0300

    Задачник(уровень 2.2) №1

--- Задачник.js
+++ Задачник.js
@@ -1,8 +1,17 @@
 'use Strict';
 
-let arr = [1, 2, 3, 4, 5, 6];
-let res = arr[0] + arr[1] + arr[2];
-console.log(res);
+let arr = [1, -2, 3, -4, 5, -6];
+
+function func() {
+   let res = 0;
+   for (let elem of arr) {
+      if (elem < 0) {
+         res++
+      }
+   }
+   return res;
+}
+console.log(func(arr));
 
 
 
