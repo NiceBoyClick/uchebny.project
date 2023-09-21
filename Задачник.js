01adbd35a7327a375810ff6225e769840f6b08e3
Константин <glass.ocean10@gmail.com>
2023-09-21 13:20:42 +0300

    Задачник(уровень 2.2) №4

--- Задачник.js
+++ Задачник.js
@@ -1,10 +1,10 @@
 'use Strict';
 
-let str = 'stringg';
-let arr = str.split('')
-let res = arr.splice(5, 1);
-console.log(arr.join(''));
-
+let arr = [1, 2, 3, 4, 5, 6];
+let sum1 = arr[0] + arr[1] + arr[2];
+let sum2 = arr[3] +  arr[4] + arr[5];
+let res = sum1 / sum2;
+console.log(res);
 
 
 
