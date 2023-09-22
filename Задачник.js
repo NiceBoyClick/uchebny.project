fb5415c428fdedcb85527d1263c2170d5efe5d5d
Константин <glass.ocean10@gmail.com>
2023-09-22 17:22:31 +0300

    Задачник(уровень 2.3) №1

--- Задачник.js
+++ Задачник.js
@@ -1,10 +1,12 @@
 'use Strict';
 
-let arr = [1, 2, 3, 4, 5, 6];
-let sum1 = arr[0] + arr[1] + arr[2];
-let sum2 = arr[3] +  arr[4] + arr[5];
-let res = sum1 / sum2;
-console.log(res);
+let word1 = "слово";
+let word2 = "олово";
+
+let res1 = word1.charAt(4);
+let res2 = word2.charAt(0);
+
+(res1 === res2) ? console.log('равны') : console.log('неравны');
 
 
 
