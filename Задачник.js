9b937fa246101580300945bd3faf05e94952d8f3
Костя <glass.ocean10@gmail.com>
2023-10-03 12:44:52 +0300

    урок 32(Коллекции Map в JavaScript) №1

--- Задачник.js
+++ Задачник.js
@@ -1,14 +1,14 @@
-'use Strict';
-
-let date = '2025-12-31';
-
-function objDate() {
-    let obj = {};
-    let arrDate = date.split('-');
-    obj.year = arrDate[0];
-    obj.month = arrDate[1];
-    obj.day = arrDate[2];
-    return obj;
-}
-
-console.log(objDate(date));
+// 'use Strict';
+//
+// let date = '2025-12-31';
+//
+// function objDate() {
+//     let obj = {};
+//     let arrDate = date.split('-');
+//     obj.year = arrDate[0];
+//     obj.month = arrDate[1];
+//     obj.day = arrDate[2];
+//     return obj;
+// }
+//
+// console.log(objDate(date));
