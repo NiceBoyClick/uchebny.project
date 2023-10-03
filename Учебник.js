c803ce8008fb6e8a825e03588463f69174d2a2f2
Костя <glass.ocean10@gmail.com>
2023-10-03 13:01:20 +0300

    урок 35(Ключи и значения в коллекциях Map) №1

--- Учебник.js
+++ Учебник.js
@@ -1,19 +1,20 @@
 'use Strict';
 
 let map = new Map;
+let keys = map.keys();
 
 let arr1 = ['string1'];
 let arr2 = ['string2'];
 let arr3 = ['string3'];
 
-let key1 = {};
-let key2 = {};
-let key3 = {};
+let key1 = {'key': 1};
+let key2 = {'key': 2};
+let key3 = {'key': 3};
 
 map.set(key1, arr1);
 map.set(key2, arr2);
 map.set(key3, arr3);
 
-console.log(map.get(key1));
-console.log(map.get(key2));
-console.log(map.get(key3));
+for (keys of map) {
+    console.log(keys);
+}
