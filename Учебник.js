384f32c312068c3f2db00a05da5163f37a0f6d25
Костя <glass.ocean10@gmail.com>
2023-10-03 12:50:43 +0300

    урок 32(Коллекции Map в JavaScript) №2

--- Учебник.js
+++ Учебник.js
@@ -1,15 +1,19 @@
 'use Strict';
 
-let  map = new Map;
+let map = new Map;
 
 let arr1 = ['string1'];
 let arr2 = ['string2'];
-let arr3 = ['string2'];
+let arr3 = ['string3'];
 
-map.set(arr1, 'data1');
-map.set(arr2, 'data2');
-map.set(arr3, 'data3');
+let key1 = {};
+let key2 = {};
+let key3 = {};
 
-console.log(map.get(arr1));
-console.log(map.get(arr2));
-console.log(map.get(arr3));
+map.set(key1, arr1);
+map.set(key2, arr2);
+map.set(key3, arr3);
+
+console.log(map.get(key1));
+console.log(map.get(key2));
+console.log(map.get(key3));
