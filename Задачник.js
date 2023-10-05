cceb1d2701af86e8e7b50171e881d2e290967672
Костя <glass.ocean10@gmail.com>
2023-10-05 15:45:02 +0300

    Задачник(уровень 2.4) №1

--- Задачник.js
+++ Задачник.js
@@ -1,14 +1,15 @@
-// 'use Strict';
-//
-// let date = '2025-12-31';
-//
-// function objDate() {
-//     let obj = {};
-//     let arrDate = date.split('-');
-//     obj.year = arrDate[0];
-//     obj.month = arrDate[1];
-//     obj.day = arrDate[2];
-//     return obj;
-// }
-//
-// console.log(objDate(date));
+'use Strict';
+
+let str = 'fjtuk6kd941';
+
+function getNum(str) {
+let arr = str.split('');
+    for (let i = 0; i < arr.length; i++) {
+        if(!isNaN(arr[i])) {
+            console.log(i);
+            break;
+        }
+    }
+}
+
+getNum(str);
