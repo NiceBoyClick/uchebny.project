9ac7232d26cd3dade0cb5eddf35df7fdce76f83e
Константин <glass.ocean10@gmail.com>
2023-10-09 09:15:04 +0300

    Коллекции Set в JavaScript №1

--- Учебник.js
+++ Учебник.js
@@ -1,30 +1,10 @@
 'use strict';
 
-let savedNumbers = new Map();
+let set = new Set;
 
-const inputs = document.querySelectorAll(".number-input");
-
-// Функция, вызываемая при нажатии Enter
-function handleEnter(event) {
-    if (event.key === "Enter") {
-        let value = parseFloat(event.target.value);
-        if (!isNaN(value)) {
-            if (!savedNumbers.has(event.target)) {
-                savedNumbers.set(event.target, []);
-            }
-            savedNumbers.get(event.target).push(value);
-            event.target.value = '';
-        }
-    }
-}
-
-// Функция, вызываемая при потере фокуса
-function handleBlur() {
-    console.log(savedNumbers);
-}
-
-inputs.forEach(input => {
-    input.addEventListener("keydown", handleEnter);
-    input.addEventListener("blur", handleBlur);
-});
-
+set.add(1);
+set.add(2);
+set.add(2);
+set.add(3);
+set.add(3);
+console.log(set);
