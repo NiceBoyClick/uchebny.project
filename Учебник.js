52f21ea9d09e9fa0e4809a827989229205dc33a7
Константин <glass.ocean10@gmail.com>
2023-10-09 09:26:04 +0300

    урок 40(Перебор коллекций Set циклом) №1

--- Учебник.js
+++ Учебник.js
@@ -1,5 +1,13 @@
 'use strict';
 
-let set = new Set([1, 2, 3]);
-console.log(set.has(3));
-console.log(set.has(4));
+let set = new Set;
+let sum = 0;
+
+set.add(1);
+set.add(2);
+set.add(3);
+
+for (let elem of set) {
+   sum += elem;
+}
+console.log(sum);
