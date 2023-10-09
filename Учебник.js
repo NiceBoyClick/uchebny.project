3b644aa83e3d0194070e2566e47ac56455f0539a
Константин <glass.ocean10@gmail.com>
2023-10-09 09:32:33 +0300

    урок 41(Преобразование коллекций Set) №1

--- Учебник.js
+++ Учебник.js
@@ -1,13 +1,5 @@
 'use strict';
 
-let set = new Set;
-let sum = 0;
-
-set.add(1);
-set.add(2);
-set.add(3);
-
-for (let elem of set) {
-   sum += elem;
-}
-console.log(sum);
+let set = new Set([1, 2, 3]);
+let arr = [...set];
+console.log(arr);
