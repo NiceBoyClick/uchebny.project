8b32f84c2abf6449812ef404d616aa6a33227d6a
Константин <glass.ocean10@gmail.com>
2023-10-10 11:24:21 +0300

    урок 48(Преобразование к массиву в JavaScript) №2

--- Учебник.js
+++ Учебник.js
@@ -2,30 +2,9 @@
 
 let elems = document.querySelectorAll('p');
 
+let arr = [...elems];
 
-// первый способ
-let arr = [];
-
-for (let elem of elems) {
-    arr.push(elem);
-}
-
-console.log(Array.isArray(arr));
-console.log(arr);
-
-
-// второй способ
-let arr1 = [...elems];
-
-console.log(Array.isArray(arr1));
-console.log(arr1);
-
-
-// третий способ
-let arr2 = Array.from(elems);
-
-console.log(Array.isArray(arr2));
-console.log(arr2);
+console.log(arr.slice(1, 4));
 
 
 
