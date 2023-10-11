0245d692634925bd3c2cc0b178fbfc4db8fe0115
Константин <glass.ocean10@gmail.com>
2023-10-11 21:57:23 +0300

    урок 7(Введение в язык JSX в React) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 function App() {
-  function func(event) {
-    console.log(event); // объект с событием
-  }
-
   return <div>
-    <button onClick={func}>act</button>
+    <p>User</p>
   </div>;
 }
-
 
 export default App;
