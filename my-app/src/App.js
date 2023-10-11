f8a05d3057d42e3278aa81398413862d78f54bf6
Константин <glass.ocean10@gmail.com>
2023-10-11 21:59:24 +0300

    урок 7(Введение в язык JSX в React) №3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function App() {
   return <div>
-    <p>User</p>
-    <p>User</p>
-    <p>User</p>
-    <p>User</p>
+    <p className="eee">User</p>
+    <p className="bbb">User</p>
+    <p className="ccc">User</p>
+    <p className="ddd">User</p>
   </div>;
 }
 
