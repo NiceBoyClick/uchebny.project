e38533f6d385130b296b670b39926bee991b2afe
Константин <glass.ocean10@gmail.com>
2023-10-12 13:47:59 +0300

    урок 9(Правила возврата функцией при сносе тега вниз в JSX) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
 function App() {
-  return	<ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
-  </ul>;
+  return(
+  <ul>
+    <li>text1</li>
+    <li>text2</li>
+    <li>text3</li>
+  </ul>
+);
 }
 
 export default App;
