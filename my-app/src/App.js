95c7b6f067b4bdf456249e561f0f13661303de22
Константин <glass.ocean10@gmail.com>
2023-10-12 14:29:48 +0300

    урок 14(Нюансы при вставке значений переменных) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function App() {
-  const str1 = 'text1';
-  const str2 = 'text1';
+  const name = 'user';
+  const age  = '30';
 
   return <div>
-    <p>{str1}</p>
-    <p>{str2}</p>
+    name: {name}
+    age: {age}
   </div>;
 }
 export default App;
