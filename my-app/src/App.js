b37f33186ca6188f3fca82ed0db75ffbbd463f2e
Константин <glass.ocean10@gmail.com>
2023-10-12 14:10:13 +0300

    урок 10(Особенности возврата нескольких тегов в JSX) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 function App() {
   return(
+  <>
   <ul>
     <li>text1</li>
     <li>text2</li>
     <li>text3</li>
   </ul>
-);
+  <ul>
+    <li>text1</li>
+    <li>text2</li>
+    <li>text3</li>
+  </ul>;
+  </>
+  )
 }
 
 export default App;
