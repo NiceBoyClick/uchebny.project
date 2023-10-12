0dee5791d19e5c506d25c67d1f81a74fc0a06792
Константин <glass.ocean10@gmail.com>
2023-10-12 13:46:27 +0300

    урок 8(Возврат вложенных тегов JSX функциями) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function App() {
-  return <div>
-    <p className="eee">User</p>
-    <p className="bbb">User</p>
-    <p className="ccc">User</p>
-    <p className="ddd">User</p>
-  </div>;
+  return	<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+  </ul>;
 }
 
 export default App;
