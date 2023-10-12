5208671fe40788d7f3a8441542f19c87342fb9f0
Константин <glass.ocean10@gmail.com>
2023-10-12 14:12:07 +0300

    урок 11(Возврат незакрытого тега JSX) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,7 @@
 import React from 'react';
 
 function App() {
-  return(
-  <>
-  <ul>
-    <li>text1</li>
-    <li>text2</li>
-    <li>text3</li>
-  </ul>
-  <ul>
-    <li>text1</li>
-    <li>text2</li>
-    <li>text3</li>
-  </ul>;
-  </>
-  )
-}
+  return <input/>;
+    }
 
 export default App;
