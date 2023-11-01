44cacb18a5c1c19a8cfe89eb6ad959685d81f3e5
NiceBoyClick <glass.ocean10@gmail.com>
2023-11-01 10:50:12 +0300

    урок 15(Вставка массивов в JSX) №1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 function App() {
-  const name = 'user';
-  const age  = '30';
+    const arr = [1, 2, 3, 4, 5];
 
-  return <div>
-    name: {name}
-    age: {age}
-  </div>;
+return <>
+    <ul>
+        <li>{arr[0]}</li>
+        <li>{arr[1]}</li>
+        <li>{arr[2]}</li>
+        <li>{arr[3]}</li>
+        <li>{arr[4]}</li>
+    </ul>
+</>;
 }
 export default App;
