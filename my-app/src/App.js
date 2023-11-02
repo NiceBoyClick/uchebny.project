a37d1e9302a07ac876fc96e9cc90b403ac1478a7
NiceBoyClick <glass.ocean10@gmail.com>
2023-11-02 10:26:45 +0300

    урок 23 (Корректность верстки в React) №2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 function App() {
-    return <tbody>
-        <ul>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-            <li>text</li>
-        </ul>
-        </tbody>
+    return <div>
+        <table border="1" width="300" height="200">
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>4</td>
+                <td>4</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>4</td>
+                <td>4</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 }
 
 export default App;
